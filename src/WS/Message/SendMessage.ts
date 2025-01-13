0fb26266b828e16f3d3988959ce6dc37e1b4133c
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-14 04:00:04 +0900

    [add] - 受け取ったメッセージで新着を設定できるように

--- src/WS/Message/SendMessage.ts
+++ src/WS/Message/SendMessage.ts
@@ -1,8 +1,20 @@
+import { setStoreHasNewMessage, storeHasNewMessage } from "~/stores/HasNewMessage";
 import { addMessage, storeHistory } from "~/stores/History";
 import type { IMessage } from "~/types/Message";
 
 export default function WSSendMessage(dat: IMessage) {
-  console.log("WSSendMessage :: triggered dat->", dat);
+  console.log("WSSendMessage :: triggered dat->", dat, location.pathname);
 
+  //履歴に追加
   addMessage(dat);
+
+  //もし受け取ったメッセージのチャンネルにいないなら新着設定
+  if (!location.pathname.includes(dat.channelId)) {
+    setStoreHasNewMessage((hnm) => {
+      return {
+        ...hnm,
+        [dat.channelId]: true,
+      };
+    });
+  }
 }
