a69d6c7b209a0d98ad2753f1b6cc61fef137c1ff
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-18 03:47:03 +0900

    [add] - チャンネルブラウザでヘッダ追加

--- src/routes/channel-browser.tsx
+++ src/routes/channel-browser.tsx
@@ -1,6 +1,13 @@
+import { Card } from "~/components/ui/card";
+import { SidebarTrigger } from "~/components/ui/sidebar";
+
 export default function ChannelBrowser() {
   return (
-    <div>
+    <div class="p-2">
+      <Card class="w-full py-3 px-5 flex items-center gap-2">
+        <SidebarTrigger />
+        <p>チャンネルブラウザ</p>
+      </Card>
       ここでチャンネル一覧を表示
     </div>
   )
