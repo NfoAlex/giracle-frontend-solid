adf57cc3e5815947dbe3293528924530f9c959a1
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-19 03:36:10 +0900

    [add] - チャンネル作成用APIハンドラ作成

---
+++ src/api/CHANNEL/CHANNEL_CREATE.ts
@@ -0,0 +1,24 @@
+export default async function POST_CHANNEL_CREATE(
+  _channelName: string,
+  _description: string,
+): Promise<{
+  message: `Channel created`;
+  data: {
+    channelId: string
+  };
+}> {
+  const res = await fetch("/api/channel/create", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      channelName: _channelName,
+      description: _description,
+    }),
+  }).catch((err) => {
+    throw new Error("CHANNEL_CREATE :: err->", err);
+  });
+  if (!res.ok) throw new Error(await res.text());
+  return res.json();
+}
