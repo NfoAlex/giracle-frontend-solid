2baa3485408b41a785a2a629a29496d4fdb7d983
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-19 03:22:53 +0900

    [add] - チャンネル参加と退出処理の実装

--- src/routes/channel-browser.tsx
+++ src/routes/channel-browser.tsx
@@ -1,14 +1,47 @@
 import { createSignal, For, onMount, Show } from "solid-js";
+import POST_CHANNEL_JOIN from "~/api/CHANNEL/CHANNEL_JOIN";
+import POST_CHANNEL_LEAVE from "~/api/CHANNEL/CHANNEL_LEAVE";
 import { GET_CHANNEL_LIST } from "~/api/CHANNEL/CHANNEL_LIST";
 import CreateChannel from "~/components/ChannelBrowser/CreateChannel";
+import { Button } from "~/components/ui/button";
 import { Card } from "~/components/ui/card";
 import { SidebarTrigger } from "~/components/ui/sidebar";
-import { storeMyUserinfo } from "~/stores/MyUserinfo";
+import { setStoreMyUserinfo, storeMyUserinfo } from "~/stores/MyUserinfo";
 import type { IChannel } from "~/types/Channel";
 
 export default function ChannelBrowser() {
   const [processing, setProcessing] = createSignal(true);
   const [channels, setChannels] = createSignal<IChannel[]>([]);
+
+  /**
+   * チャンネルへ参加する
+   * @param channelId 参加するチャンネルId
+   */
+  const joinChannel = (channelId: string) => {
+    POST_CHANNEL_JOIN(channelId)
+      .then((r) => {
+        console.log("ChannelBrowser :: joinChannel :: r ->", r);
+        //StoreにチャンネルIdを追加
+        setStoreMyUserinfo((prev) => ({...prev, ChannelJoin: [...prev.ChannelJoin, {channelId: channelId}]}));
+      })
+      .catch((err) => console.error("ChannelBrowser :: joinChannel :: err ->", err));
+  }
+
+  /**
+   * チャンネルから退出する
+   * @param channelId 退出するチャンネルId
+   */
+  const leaveChannel = (channelId: string) => {
+    POST_CHANNEL_LEAVE(channelId)
+      .then((r) => {
+        console.log("ChannelBrowser :: leaveChannel :: r ->", r);
+        //StoreからチャンネルIdをを削除
+        setStoreMyUserinfo((prev) => (
+          {...prev, ChannelJoin: prev.ChannelJoin.filter((cj) => cj.channelId !== channelId)}
+        ));
+      })
+      .catch((err) => console.error("ChannelBrowser :: leaveChannel :: err ->", err));
+  }
 
   onMount(async () => {
     GET_CHANNEL_LIST()
@@ -40,7 +73,15 @@ export default function ChannelBrowser() {
               <p>{channel.name}</p>
               <p class="font-thin"> | </p>
               <p>{channel.description}</p>
-              <p class="ml-auto">{ storeMyUserinfo.ChannelJoin.some((cj) => cj.channelId === channel.id) ? "参加済み" : "未参加" }</p>
+              <div class="ml-auto">
+                { 
+                  storeMyUserinfo.ChannelJoin.some((cj) => cj.channelId === channel.id)
+                  ?
+                    <Button onclick={()=>leaveChannel(channel.id)} variant={"outline"}>退出</Button>
+                  :
+                    <Button onclick={()=>joinChannel(channel.id)}>参加</Button>
+                }
+              </div>
             </Card>
           )}
         </For>
