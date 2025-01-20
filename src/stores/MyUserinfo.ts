55a2b166d15dd415f4396ef4bda4564a5274eada
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-21 03:49:44 +0900

    [add] - 自分のロールから権限を取得するハンドラ作成

--- src/stores/MyUserinfo.ts
+++ src/stores/MyUserinfo.ts
@@ -1,5 +1,6 @@
 import { createStore } from "solid-js/store";
 import type { IUser } from "~/types/User";
+import { getterRoleInfo } from "./RoleInfo";
 
 export const [storeMyUserinfo, setStoreMyUserinfo] = createStore<IUser>({
   id: "",
@@ -8,3 +9,21 @@ export const [storeMyUserinfo, setStoreMyUserinfo] = createStore<IUser>({
   ChannelJoin: [],
   RoleLink: [],
 });
+
+/**
+ * 該当の権限を持っているか確認する
+ * @param roleTerm 確認する権限名
+ */
+export const getRolePower = async (
+  roleTerm: "manageRole" | "manageChannel" | "manageRole" | "manageUser"
+): Promise<boolean> => {
+  for (const index in storeMyUserinfo.RoleLink) {
+    const role = await getterRoleInfo(storeMyUserinfo.RoleLink[index].roleId);
+
+    if (role[roleTerm]) {
+      return true;
+    }
+  }
+
+  return false;
+}
