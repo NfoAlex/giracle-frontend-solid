9a4e775465d2835891cc443d25274022c16186fc
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-22 01:14:31 +0900

    [change] - ロール情報の取得方法を改善、あとポップオーバー追加

--- src/components/unique/RoleChip.tsx
+++ src/components/unique/RoleChip.tsx
@@ -3,16 +3,12 @@ import { Badge } from "../ui/badge";
 import { getterRoleInfo } from "~/stores/RoleInfo";
 
 export default function RoleChip(props: { deltable: boolean, roleId: string }) {
-  const role = getterRoleInfo(props.roleId);
-  
   return (
-    <div>
-      <Badge variant="outline">
-        <span class="flex items-center gap-1">
-          <IconCircleFilled size={12} color={ role.color } />
-          <p>{ role.name }</p>
-        </span>
-      </Badge>
-    </div>
+    <Badge variant="outline">
+      <span class="flex items-center gap-1">
+        <IconCircleFilled size={12} color={ getterRoleInfo(props.roleId).color } />
+        <p>{ getterRoleInfo(props.roleId).name }</p>
+      </span>
+    </Badge>
   )
 }
