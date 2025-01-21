4f3fba6ad1433bbf563bb6a636f351e1d0fe54f9
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-22 00:22:48 +0900

    [add] - ユーザーダイアログの土台作成

--- src/components/unique/UserName.tsx
+++ src/components/unique/UserName.tsx
@@ -1,8 +1,50 @@
-import { createResource } from "solid-js";
+import { createResource, createSignal, For, Show } from "solid-js";
 import { getterUserinfo } from "~/stores/Userinfo";
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "../ui/dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Card } from "../ui/card";
+import { Label } from "../ui/label";
 
 export default function UserName(props: { userId: string }) {
   const [user] = createResource(props.userId, getterUserinfo);
+  const [open, setOpen] = createSignal(false);
 
-  return <p class="font-bold">{user.loading ? props.userId : user()?.name}</p>;
+  return (
+    <Dialog open={open()} onOpenChange={setOpen}>
+      <DialogContent class="p-0 max-h-[50vh] flex flex-col overflow-y-auto">
+        <Show when={open() && !user.loading}>
+          <div class="h-[45%] w-full">
+            <img
+              alt="user banner"
+              src={`/api/user/banner/${user()?.id}`}
+              class="h-full w-full object-cover"
+            />
+          </div>
+          <div class="w-full -mt-12">
+            <Avatar class="w-16 h-16 ml-4">
+              <AvatarImage src={`/api/user/icon/${user()?.id}`} />
+              <AvatarFallback class="w-full h-full">{user()?.name}</AvatarFallback>
+            </Avatar>
+          </div>
+
+          <div class="pb-2 px-4 flex flex-col gap-4">
+            <p class="font-bold text-2xl w-min">{user()?.name}</p>
+
+            <Label>自己紹介</Label>
+            <Card class="px-4 py-2">{user()?.selfIntroduction}</Card>
+
+            <Label>ロール</Label>
+            <Card class="px-4 py-2">
+              <For each={user()?.RoleLink}>
+                {(role) => <p>{role.roleId}</p>}
+              </For>
+            </Card>
+          </div>
+        </Show>
+      </DialogContent>
+      <DialogTrigger>
+        <p class="font-bold">{user.loading ? props.userId : user()?.name}</p>
+      </DialogTrigger>
+    </Dialog>
+  );
 }
