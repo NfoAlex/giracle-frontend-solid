e462357b4bd67ec6ef217a42c986788e04fd4f23
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-22 01:42:37 +0900

    [change] - ロール更新のWSハンドラを改善

--- src/WS/Role/RoleLinked.ts
+++ src/WS/Role/RoleLinked.ts
@@ -1,15 +1,31 @@
 import { setStoreMyUserinfo, storeMyUserinfo } from "~/stores/MyUserinfo";
+import { setStoreUserinfo } from "~/stores/Userinfo";
 
 export default function WSRoleLinked(dat: { roleId: string, userId: string }) {
   console.log("WSRoleLinked :: triggered dat->", dat);
 
-  const myRole = storeMyUserinfo.RoleLink;
-
-  //リンクされておらず自分のユーザーId宛てならリンクする
-  if (!Object.keys(myRole).includes(dat.roleId) && dat.userId === storeMyUserinfo.id) {
+  //自分に対する付与なら自分のStoreを更新
+  if (storeMyUserinfo.id === dat.userId) {
     setStoreMyUserinfo((prev) => {
       prev.RoleLink.push({ roleId: dat.roleId });
       return prev;
     });
   }
+
+  //ユーザー情報Storeを更新
+  setStoreUserinfo((prev) => {
+    //もしユーザー情報Storeにこの人が無いなら停止
+    if (prev[dat.userId] === undefined) return prev;
+    //ユーザー情報をコピー
+    const _user = { ...prev[dat.userId] };
+    //すでにロールがリンクされているなら停止
+    if (_user.RoleLink.some((rl) => rl.roleId === dat.roleId)) return prev;
+    //リンク
+    _user.RoleLink.push({ roleId: dat.roleId });
+    //Store更新
+    prev[dat.userId] = _user;
+
+    console.log("WSRoleLinked :: setStoreUserinfo :: prev[dat.userId] ->", prev[dat.userId]);
+    return prev;
+  });
 }
--- src/WS/Role/RoleUnlinked.ts
+++ src/WS/Role/RoleUnlinked.ts
@@ -1,4 +1,5 @@
 import { setStoreMyUserinfo, storeMyUserinfo } from "~/stores/MyUserinfo";
+import { setStoreUserinfo } from "~/stores/Userinfo";
 
 export default function WSRoleUnlinked(dat: { roleId: string, userId: string }) {
   console.log("WSRoleUnlinked :: triggered dat->", dat);
@@ -6,10 +7,27 @@ export default function WSRoleUnlinked(dat: { roleId: string, userId: string }) 
   const myRole = storeMyUserinfo.RoleLink;
 
   //リンクされていて自分のユーザーId宛てなら解除
-  if (Object.keys(myRole).includes(dat.roleId) && dat.userId === storeMyUserinfo.id) {
+  if (storeMyUserinfo.id === dat.userId) {
     setStoreMyUserinfo((prev) => {
       prev.RoleLink.filter((role) => role.roleId !== dat.roleId);
       return prev;
     });
   }
+
+  //ユーザー情報Storeを更新
+  setStoreUserinfo((prev) => {
+    //もしユーザー情報Storeにこの人が無いなら停止
+    if (prev[dat.userId] === undefined) return prev;
+    //ユーザー情報をコピー
+    const _user = { ...prev[dat.userId] };
+    //ロールが無ければ停止
+    if (!(_user.RoleLink.some((rl) => rl.roleId === dat.roleId))) return prev;
+    //解除
+    _user.RoleLink.filter((role) => role.roleId !== dat.roleId);
+    //Store更新
+    prev[dat.userId] = _user;
+
+    console.log("WSRoleUnlinked :: setStoreUserinfo :: prev[dat.userId] ->", prev[dat.userId]);
+    return prev;
+  });
 }
