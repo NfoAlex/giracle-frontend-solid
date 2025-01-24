50037d1d04f7c0a820abea4034b8c34379d72c59
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-24 19:22:51 +0900

    [add] - ファイルプレビューをできるように

--- src/components/Channel/ChannelContent/MessageRender.tsx
+++ src/components/Channel/ChannelContent/MessageRender.tsx
@@ -1,9 +1,10 @@
-import { Show } from "solid-js";
+import {For, Show} from "solid-js";
 import UserName from "~/components/unique/UserName";
 import type { IMessage } from "~/types/Message";
 import MessageTextRender from "./MessageRender/MessageTextRender";
 import URLPreview from "~/components/Channel/ChannelContent/MessageRender/URLPreview";
 import SystemMessageRender from "~/components/Channel/ChannelContent/MessageRender/SystemMessageRender";
+import FilePreview from "~/components/Channel/ChannelContent/MessageRender/FilePreview";
 
 export default function MessageRender(props: {
   message: IMessage;
@@ -24,6 +25,15 @@ export default function MessageRender(props: {
         }
 
         { (props.message.MessageUrlPreview.length > 0) && <URLPreview MessageUrlPreview={props.message.MessageUrlPreview} />}
+        {
+          props.message.MessageFileAttached.length > 0
+          &&
+          (
+            <For each={props.message.MessageFileAttached}>
+              {(f) => <FilePreview file={f} />}
+            </For>
+          )
+        }
       </div>
     </div>
   );
