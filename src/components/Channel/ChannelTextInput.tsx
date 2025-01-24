408884acce0598e5ade0c40303553a9c1ec6b697
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-24 17:35:35 +0900

    [change] - ファイル受け取り部分の作成

--- src/components/Channel/ChannelTextInput.tsx
+++ src/components/Channel/ChannelTextInput.tsx
@@ -3,10 +3,12 @@ import { TextField, TextFieldInput } from "../ui/text-field";
 import { Button } from "../ui/button";
 import { useParams } from "@solidjs/router";
 import POST_MESSAGE_SEND from "~/api/MESSAGE/MESSAGE_SEND";
+import {IconFile, IconUpload} from "@tabler/icons-solidjs";
 
 export default function ChannelTextInput() {
   const params = useParams();
   const [text, setText] = createSignal("");
+  const [fileInput, setFileInput] = createSignal<File[]>([]);
 
   const sendMsg = () => {
     console.log("ChannelTextInput :: sendMsg : params.id->", {...params});
@@ -20,15 +22,51 @@ export default function ChannelTextInput() {
         console.error("POST_MESSAGE_SEND :: e->", e);
       });
   }
-  
+
+  /**
+   * ファイル選択ダイアログを開く
+   */
+  const bindFiles = () => {
+    const fileInputEl = document.getElementById("fileInput") as HTMLInputElement;
+    fileInputEl.click();
+    fileInputEl.onchange = (e) => {
+      const files = (e.target as HTMLInputElement).files;
+      if (files) {
+        setFileInput([...files]);
+        console.log("ChannelTextInput :: bindFiles : fileInput->", fileInput());
+      }
+    }
+  }
+
+  const receiveFiles = (event: ClipboardEvent) => {
+    const items = event.clipboardData?.items;
+    if (items) {
+      const files = [];
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].kind === "file") {
+          const file = items[i].getAsFile();
+          if (file) {
+            files.push(file);
+          }
+        }
+      }
+      setFileInput([...fileInput(), ...files]);
+      console.log("ChannelTextInput :: receiveFiles : fileInput->", fileInput());
+    }
+  }
+
   return (
     <div class="flex items-center gap-1">
+      <input type={"file"} id={"fileInput"} class={"hidden"} />
+
+      <Button onClick={bindFiles} variant={"secondary"}><IconUpload /></Button>
       <TextField class="grow">
         <TextFieldInput
           type="text"
           value={text()}
           onInput={(e) => setText(e.currentTarget.value)}
           onKeyDown={(e) => {e.key === "Enter" && sendMsg()}}
+          onPaste={(e) => receiveFiles(e)}
         />
       </TextField>
       <Button onClick={sendMsg}>送信</Button>
