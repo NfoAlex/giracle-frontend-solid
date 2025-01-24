6f1334a1d23a41d4c640cbbc0e99c6d803c7717d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-24 15:08:03 +0900

    [change] - URLプレビュー用のCSS調整

--- src/components/Channel/ChannelContent/MessageRender/URLPreview.tsx
+++ src/components/Channel/ChannelContent/MessageRender/URLPreview.tsx
@@ -4,11 +4,11 @@ import {For} from "solid-js";
 
 export default function URLPreview(props: { MessageUrlPreview: IMessageUrlPreview[] }) {
   return (
-    <div class={"py-1"}>
+    <div class={"py-1 flex flex-col gap-1"}>
       <For each={props.MessageUrlPreview}>
         {(urlPreview: IMessageUrlPreview) => (
           <Card class={"md:w-1/3 sm:w-1/2"}>
-            <img src={urlPreview.imageLink} alt={""} />
+            <img src={urlPreview.imageLink} alt={""} class={"rounded-t"} />
             <CardHeader class={"flex flex-row items-center gap-1"}>
               <img class={"w-4 h-fit"} src={urlPreview.faviconLink} alt="favicon" />
               <CardTitle>{ urlPreview.title }</CardTitle>
