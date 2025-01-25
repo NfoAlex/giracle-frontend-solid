c153093890cb56f92ec43f82e1011fce48243345
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-26 05:33:42 +0900

    [fix] - WSのメッセージ削除ハンドラが動作してなかった

--- src/WS/Message/MessageDelete.ts
+++ src/WS/Message/MessageDelete.ts
@@ -1,13 +1,24 @@
-import { setStoreHistory } from "~/stores/History";
-import type { IMessage } from "~/types/Message";
+import {setStoreHistory, storeHistory} from "~/stores/History";
+import type {IMessage} from "~/types/Message";
 
 export default function WSMessageDeleted(dat: { messageId: IMessage["id"], channelId: string }) {
-  //console.log("WSMessageDeleted :: triggered dat->", dat);
+  console.log("WSMessageDeleted :: triggered dat->", dat);
 
   //履歴から削除
   setStoreHistory((prev) => {
-    const his = prev[dat.channelId].history.filter((m) => m.id !== dat.messageId);
-    prev[dat.channelId].history = his;
-    return prev;
+    console.log("WSMessageDeleted :: setStoreHistory : 削除するメッセ->", prev[dat.channelId].history.find((m) => m.id === dat.messageId));
+
+    // 新しいオブジェクトを作成して返す
+    const newHistory = prev[dat.channelId].history.filter((m) => m.id !== dat.messageId);
+
+    return {
+      ...prev,
+      [dat.channelId]: {
+        ...prev[dat.channelId],
+        history: newHistory
+      }
+    };
   });
+
+  console.log("WSMessageDeleted :: setStoreHistory : 削除するできた？->", storeHistory[dat.channelId].history.find((m) => m.id === dat.messageId));
 }
