4a3f65a734820d6cd448ff58069de242ea21433f
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-26 05:33:54 +0900

    [add] - メッセージの削除をできるように

--- src/components/Channel/ChannelContent/HoverMenu.tsx
+++ src/components/Channel/ChannelContent/HoverMenu.tsx
@@ -1,6 +1,7 @@
 import {Card} from "~/components/ui/card";
 import {IconTrash} from "@tabler/icons-solidjs";
 import {Button} from "@kobalte/core/button";
+import DELETE_MESSAGE_DELETE from "~/api/MESSAGE/MESSAGE_DELETE";
 
 export default function HoverMenu(props: { messageId: string }) {
 
@@ -8,11 +9,17 @@ export default function HoverMenu(props: { messageId: string }) {
    * メッセージの削除
    */
   const deleteMessage = () => {
-
+    DELETE_MESSAGE_DELETE(props.messageId)
+      .then((r) => {
+        console.log("DELETE_MESSAGE_DELETE :: r->", r);
+      })
+      .catch((e) => {
+        console.error("DELETE_MESSAGE_DELETE :: e->", e);
+      });
   }
   return (
     <Card class={"p-2 flex items-center"}>
-      <Button><IconTrash /></Button>
+      <Button ondblclick={deleteMessage}><IconTrash color={"red"} /></Button>
     </Card>
   )
 }
