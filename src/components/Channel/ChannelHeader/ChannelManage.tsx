a9a79fde99b34b7b69cc9b4e2400d6a9cad3fe86
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-28 00:42:39 +0900

    [change] - ボタンの見た目

--- src/components/Channel/ChannelHeader/ChannelManage.tsx
+++ src/components/Channel/ChannelHeader/ChannelManage.tsx
@@ -1,19 +1,17 @@
-import {Dialog} from "@kobalte/core/dialog";
-import {DialogContent, DialogDescription, DialogHeader, DialogTrigger} from "~/components/ui/dialog";
-import {Button} from "@kobalte/core/button";
+import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTrigger} from "~/components/ui/dialog";
 import {directGetterChannelInfo} from "~/stores/ChannelInfo";
 import {Card} from "~/components/ui/card";
 import {getRolePower} from "~/stores/MyUserinfo";
-import {IconCancel, IconCheck, IconPencil, IconX} from "@tabler/icons-solidjs";
+import {IconCheck, IconPencil, IconX} from "@tabler/icons-solidjs";
 import {createSignal} from "solid-js";
 import {TextField, TextFieldInput, TextFieldTextArea} from "~/components/ui/text-field";
-import {TextArea} from "@kobalte/core/text-field";
+import {Button} from "~/components/ui/button";
 
 export default function ChannelManage(props: {channelId: string}) {
   const [editName, setEditName] = createSignal(false);
   const [editDescription, setEditDescription] = createSignal(false);
-  const [newName, setNewName] = createSignal("");
-  const [newDescription, setNewDescription] = createSignal("");
+  const [, setNewName] = createSignal("");
+  const [, setNewDescription] = createSignal("");
 
   return (
     <Dialog>
@@ -33,13 +31,13 @@ export default function ChannelManage(props: {channelId: string}) {
                     onInput={(e)=>setNewName(e.currentTarget.value)}
                   />
                 </TextField>
-                <Button class={"ml-auto"}><IconCheck /></Button>
-                <Button onClick={()=>setEditName(false)} class={"ml-auto"}><IconX /></Button>
+                <Button class={"ml-auto h-10 w-10"}><IconCheck /></Button>
+                <Button onClick={()=>setEditName(false)} class={"ml-auto h-10 w-10"} variant={"outline"}><IconX /></Button>
               </div>
             :
               <p class={"text-2xl truncate"}>{ directGetterChannelInfo(props.channelId).name ?? "ロード中..." }</p>
           }
-          { (getRolePower("manageChannel") && !editName()) && <Button onClick={()=>setEditName(true)} class={"ml-auto border p-2 rounded-md"}><IconPencil /></Button> }
+          { (getRolePower("manageChannel") && !editName()) && <Button onClick={()=>setEditName(true)} variant={"outline"} class={"ml-auto border rounded-md h-10 w-10"}><IconPencil /></Button> }
         </DialogHeader>
         <DialogDescription>
           {
@@ -49,20 +47,20 @@ export default function ChannelManage(props: {channelId: string}) {
                   <p>{ directGetterChannelInfo(props.channelId).description ?? "概要" }</p>
                 { directGetterChannelInfo(props.channelId).description==="" && <p class={"text-muted-foreground"}>概要が空です。</p> }
 
-                  <Button onClick={()=>setEditDescription(true)} class={"absolute bottom-2 right-2 p-2 border rounded-md"} ><IconPencil /></Button>
+                  <Button onClick={()=>setEditDescription(true)} class={"absolute bottom-2 right-2 border rounded-md h-10 w-10"} variant={"outline"} ><IconPencil /></Button>
               </Card>
             :
-              <div class={"flex flex-col gap-2"}>
+              <div class={"flex flex-col gap-1"}>
                 <TextField class={"max-h-64 overflow-y-auto"}>
                   <TextFieldTextArea
                     value={directGetterChannelInfo(props.channelId).description}
                     onInput={(e)=>setNewDescription(e.currentTarget.value)}
                   />
                 </TextField>
-                <Card class={"w-fit ml-auto p-2 flex items-center gap-1"}>
-                  <Button onClick={()=>setEditDescription(false)} class={"border rounded-md"} ><IconCheck /></Button>
-                  <Button onClick={()=>setEditDescription(false)} class={"border rounded-md"} ><IconX /></Button>
-                </Card>
+                <div class={"w-fit ml-auto p-2 flex items-center gap-1"}>
+                  <Button onClick={()=>setEditDescription(false)} class={"border rounded-md h-10 w-10"} ><IconCheck /></Button>
+                  <Button onClick={()=>setEditDescription(false)} class={"border rounded-md h-10 w-10"} variant={"outline"} ><IconX /></Button>
+                </div>
               </div>
           }
         </DialogDescription>
