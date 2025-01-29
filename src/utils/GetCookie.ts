eb0ef566ac477a331d5b1a2e8ca2d85bcb026d74
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-29 15:05:56 +0900

    [fix] - クッキー取得関数の判別式改善

--- src/utils/GetCookie.ts
+++ src/utils/GetCookie.ts
@@ -2,7 +2,7 @@ export default function GetCookie(name: string): string | undefined {
   const cookirArr = document.cookie.split(";");
   let cookieValue: undefined | string;
   for (const c of cookirArr) {
-    if (c.startsWith(`${name}=`)) {
+    if (c.startsWith(`${name}=`) || c.startsWith(` ${name}=`)) {
       cookieValue = c.split(`${name}=`)[1];
     }
   }
