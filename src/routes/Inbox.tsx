34904635c45ded92a01976c3b72a9babde4bc631
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-31 05:13:19 +0900

    [add] - インボックスなかった時の表示追加

--- src/routes/Inbox.tsx
+++ src/routes/Inbox.tsx
@@ -1,12 +1,13 @@
 import {Card} from "~/components/ui/card";
 import {SidebarTrigger} from "~/components/ui/sidebar";
 import {storeInbox} from "~/stores/Inbox";
-import {For} from "solid-js";
+import {For, Show} from "solid-js";
 import MessageRender from "~/components/Channel/ChannelContent/MessageRender";
 import {Badge} from "~/components/ui/badge";
 import {directGetterChannelInfo} from "~/stores/ChannelInfo";
 import {Avatar, AvatarFallback, AvatarImage} from "~/components/ui/avatar";
 import {getterUserinfo} from "~/stores/Userinfo";
+import {IconBed} from "@tabler/icons-solidjs";
 
 export default function Inbox() {
   return (
@@ -17,6 +18,13 @@ export default function Inbox() {
       </Card>
 
       <div class={"flex flex-col-reverse gap-1"}>
+        <Show when={storeInbox.length === 0}>
+          <span class={"p-2 text-center flex flex-col items-center"}>
+            <IconBed size={44} />
+            <p>通知はありません</p>
+          </span>
+        </Show>
+
         <For each={storeInbox}>
           {(inboxItem) =>
             <Card class={"p-2 flex flex-col gap-2"}>
