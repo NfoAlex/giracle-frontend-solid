20a06fdcc2593f1e21b43dc1a576e46ae6b17f0c
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-30 18:39:14 +0900

    [change] - 404ページのデザイン変更

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,9 +1,17 @@
 import { A } from "@solidjs/router";
+import {IconFileBroken} from "@tabler/icons-solidjs";
+import {Button} from "~/components/ui/button";
 
 export default function NotFound() {
   return (
-    <p class="text-2xl text-center mt-10">
-      ここにはなにもありません。
-    </p>
+    <div class={"mx-auto pt-10 text-center flex flex-col items-center gap-3"}>
+      <IconFileBroken size={55} />
+
+      <p class="text-2xl font-bold mt-10">
+        ここにはなにもありません。
+      </p>
+
+      <A href={"/app/channel-browser"}><Button>トップへ戻る</Button></A>
+    </div>
   );
 }
