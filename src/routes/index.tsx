ea82ce56cbfb13de9a49e99fad8e1996649fa5c2
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-01-30 18:31:04 +0900

    [change] - "/"ページからチャンネルブラウザへリダイレクトさせるように

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,11 @@
+import {useNavigate} from "@solidjs/router";
+
 export default function Home() {
+  const navi = useNavigate();
+
+  navi("/app/channel-browser");
+
   return (
-    <div>ここがトップ</div>
+    <div class={"text-center mt-10"}>Loading...</div>
   );
 }
