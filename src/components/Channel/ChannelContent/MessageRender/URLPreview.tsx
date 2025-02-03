0b3361ac85e589e0d6bced106c573e8873f747ac
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-03 14:51:04 +0900

    [change] - おまけのURLプレビューのレイアウト改善

--- src/components/Channel/ChannelContent/MessageRender/URLPreview.tsx
+++ src/components/Channel/ChannelContent/MessageRender/URLPreview.tsx
@@ -7,8 +7,10 @@ export default function URLPreview(props: { MessageUrlPreview: IMessageUrlPrevie
     <div class={"py-1 flex flex-col gap-1"}>
       <For each={props.MessageUrlPreview}>
         {(urlPreview: IMessageUrlPreview) => (
-          <Card class={"md:w-1/2 lg:w-1/3 w-1/2"}>
-            <img src={urlPreview.imageLink} alt={urlPreview.imageLink} class={"rounded-t mx-auto border-b-2"} />
+          <Card class={"flex flex-col md:w-1/2 lg:w-1/4 sm:w-1/2 w-full"}>
+            <div class={"border-b-2 grow"}>
+              <img src={urlPreview.imageLink} alt={urlPreview.imageLink} class={"mx-auto rounded-t max-h-32 w-full object-cover"} />
+            </div>
             <CardHeader class={"flex flex-row items-center gap-1"}>
               {urlPreview.faviconLink && <img class={"w-4 h-fit shrink"} src={urlPreview.faviconLink} alt="favicon" />}
               <CardTitle>
