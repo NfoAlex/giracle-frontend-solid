48e10cf682620822236b166af90c65b53c2df268
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-05 07:55:33 +0900

    [change] - ユーザー検索APIの返り値整備

--- src/api/USER/USER_SEARCH..ts
+++ src/api/USER/USER_SEARCH..ts
@@ -1,8 +1,11 @@
+import type {IUser} from "~/types/User";
+
 export default async function GET_USER_SEARCH(
   _username: string,
   _channelId: string
 ): Promise<{
-  message: "User created";
+  message: "User search result";
+  data: IUser[]
 }> {
   const res = await fetch(`/api/user/search?username=${_username}&joinedChannel=${_channelId}`, {
     method: "GET",
