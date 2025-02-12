e1aada931482189e611490f2908a0efab4d88174
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-12 21:47:34 +0900

    [remove] - 不要なimport

--- src/routes/channel/[id].tsx
+++ src/routes/channel/[id].tsx
@@ -1,7 +1,6 @@
 import ChannelContents from "~/components/Channel/ChannelContents";
 import ChannelHeader from "~/components/Channel/ChannelHeader";
 import ChannelTextInput from "~/components/Channel/ChannelTextInput";
-import SwipeToOpenSidebarWrapper from "~/components/unique/SwipeToOpenSidebarWrapper";
 
 export default function Channel() {
   return (
