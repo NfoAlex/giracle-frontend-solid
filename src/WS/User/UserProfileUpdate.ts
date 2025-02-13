0d8006cf9203057ad1a99eb322753946d6472b5f
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-14 01:17:06 +0900

    [remove] - 不要なimport

--- src/WS/User/UserProfileUpdate.ts
+++ src/WS/User/UserProfileUpdate.ts
@@ -1,5 +1,5 @@
-import {setStoreUserOnline, updateUserinfo} from "~/stores/Userinfo";
 import type {IUser} from "~/types/User";
+import {updateUserinfo} from "~/stores/Userinfo";
 
 export default function WSUserProfileUpdate(dat: IUser) {
   console.log("WSProfileUpdate :: triggered dat->", dat);
