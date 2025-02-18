31504939151a80feff99bab420675a3139bf62f5
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-18 14:28:05 +0900

    [add] - 画像拡大用コンポ作成

---
+++ src/components/unique/ImageWithModal.tsx
@@ -0,0 +1,25 @@
+import {createSignal} from "solid-js";
+import { Dialog as KDialog } from "@kobalte/core/dialog";
+
+/**
+ * img要素として使うことができクリックするとモーダルとして拡大もできるコンポーネント。
+ * @param props - {class: 適用したいCSSのクラス, src: 画像のURL}
+ */
+export default function ImageWithModal(props: { class: string, src: string }) {
+  const [isOpen, setIsOpen] = createSignal(false);
+
+  return (
+    <>
+      <img onClick={() => setIsOpen(true)} src={props.src} class={`${props.class} cursor-pointer`}  alt={props.src}/>
+      <KDialog open={isOpen()} onOpenChange={setIsOpen}>
+        <KDialog.Portal>
+          <div class={"inset-0 z-50 p-2 fixed flex justify-center items-center w-full h-full bg-black bg-opacity-50"}>
+            <KDialog.Content class={"h-3/4 w-10/12 flex justify-center items-center"}>
+              <img onClick={()=>setIsOpen(false)} src={props.src} class={`w-fit h-fit cursor-pointer`} alt={props.src}/>
+            </KDialog.Content>
+          </div>
+        </KDialog.Portal>
+      </KDialog>
+    </>
+  )
+}
