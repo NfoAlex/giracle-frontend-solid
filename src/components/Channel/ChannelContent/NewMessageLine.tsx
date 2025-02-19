5670bb487c412183ad41ab89510f1e9b5c31a575
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-19 09:31:03 +0900

    [change] - 新着線の表示変更

--- src/components/Channel/ChannelContent/NewMessageLine.tsx
+++ src/components/Channel/ChannelContent/NewMessageLine.tsx
@@ -1,8 +1,10 @@
+import { Badge } from "~/components/ui/badge";
+
 export default function NewMessageLine () {
   return (
     <div class="w-full flex flex-row items-center gap-2" id="NEW_LINE">
       <hr class="grow shrink" />
-      <p class="shrink-0">新着</p>
+      <Badge class="shrink-0">新着</Badge>
       <hr class="grow shrink" />
     </div>
   );
