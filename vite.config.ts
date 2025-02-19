e20e6006dd80c9d58f9333e278426059cc80157c
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-19 09:29:06 +0900

    [add] - .envでホストに使うドメインとポートを設定できるように

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import solidPlugin from 'vite-plugin-solid';
 import pkg from './package.json' with { type: "json" };
 import 'dotenv/config';
 
+//.envの変数、説明については.env.sampleファイル参照
 const apiURI = process.env.VITE_CORS_ORIGIN || "http://localhost:3000";
+const prodPort: number = parseInt(process.env.VITE_PROD_PORT || "4173");
+const prodDomain: string[]  = [process.env.VITE_PROD_DOMAIN || ""];
 
 export default defineConfig({
   plugins: [
@@ -57,8 +60,8 @@ export default defineConfig({
     },
   },
   preview: {
-    port: 6173,
-    allowedHosts: []
+    port: prodPort,
+    allowedHosts: prodDomain
   },
   resolve: {
     alias: {
