7b16c9e5052ab7ee30f0747edbfee97493043b4c
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-19 14:08:25 +0900

    [change] - メッセのユーザー名表示部分にユーザー情報モーダル適用

--- src/components/Channel/ChannelContent/MessageRender.tsx
+++ src/components/Channel/ChannelContent/MessageRender.tsx
@@ -1,10 +1,11 @@
 import {For, Show} from "solid-js";
-import UserName from "~/components/unique/UserName";
 import type { IMessage } from "~/types/Message";
 import MessageTextRender from "./MessageRender/MessageTextRender";
 import URLPreview from "~/components/Channel/ChannelContent/MessageRender/URLPreview";
 import SystemMessageRender from "~/components/Channel/ChannelContent/MessageRender/SystemMessageRender";
 import FilePreview from "~/components/Channel/ChannelContent/MessageRender/FilePreview";
+import {getterUserinfo} from "~/stores/Userinfo";
+import UserinfoModalWrapper from "~/components/unique/UserinfoModalWrapper";
 
 export default function MessageRender(props: {
   message: IMessage;
@@ -13,12 +14,14 @@ export default function MessageRender(props: {
   return (
     <div class="w-full">
       <Show when={props.displayUserName}>
-        <UserName userId={props.message.userId} />
+        <UserinfoModalWrapper userId={props.message.userId}>
+          <p class="font-bold hover:underline">{getterUserinfo(props.message.userId).name}</p>
+        </UserinfoModalWrapper>
       </Show>
       <div class="flex flex-col">
         {
           props.message.isSystemMessage
-          ?
+            ?
             <SystemMessageRender content={props.message.content} />
           :
             <MessageTextRender content={props.message.content} />
