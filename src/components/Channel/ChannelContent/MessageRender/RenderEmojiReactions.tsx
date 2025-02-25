64467748fa126bbcfe3d50ba0e62f8bbd7cec2b3
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-25 23:04:48 +0900

    [change] - リアクションレンダー部分の更新

--- src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions.tsx
+++ src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions.tsx
@@ -6,21 +6,19 @@ import {createMutable} from "solid-js/store";
 
 export default function RenderEmojiReactions(props: {reaction: IMessage["reactionSummary"]}) {
   const db = new Database();
-  const emojiToRender:{ [key: string]: number } = createMutable({})
+  const emojiToRender:{ [key: string]: string } = createMutable({})
 
   onMount(async () => {
     //リアクションデータごとに絵文字をレンダーする
     for (const r of props.reaction) {
-      //コード取得
-      const code = Object.entries(r)[0][0];
       //データ取得、無ければ停止
-      const emojiData = await db.getEmojiByShortcode(code);
-      if (emojiData === null) return code;
+      const emojiData = await db.getEmojiByShortcode(r.emojiCode);
+      if (emojiData === null) return;
 
-      //console.log("RenderEmojiReactions :: getEmoji : 結果->", emojiData);
+      console.log("RenderEmojiReactions :: getEmoji : 結果->", emojiData);
       //絵文字そのものを格納
       // @ts-ignore - 参照は正常にできている
-      emojiToRender[code] = emojiData.unicode;
+      emojiToRender[r.emojiCode] = emojiData.unicode;
     }
   })
 
@@ -29,12 +27,10 @@ export default function RenderEmojiReactions(props: {reaction: IMessage["reactio
       <For each={props.reaction}>
         {
           (r)=> {
-            //JSONを配列化しｔｒ表示する
-            const reaction = Object.entries(r);
             return (
               <Card class={"p-1 text-sm flex items-center gap-1"}>
-                <span>{ emojiToRender[reaction[0][0]]!==undefined ? emojiToRender[reaction[0][0]] : reaction[0][0].slice(0,5) }</span>
-                <span>{ reaction[0][1] }</span>
+                <span>{ emojiToRender[r.emojiCode]!==undefined ? emojiToRender[r.emojiCode] : r.emojiCode.slice(0,5) }</span>
+                <span>{ r.count }</span>
               </Card>
             )
           }
