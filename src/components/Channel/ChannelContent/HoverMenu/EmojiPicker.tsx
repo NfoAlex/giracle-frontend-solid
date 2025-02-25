05324aa20b829d94bda7193e77a76e5e3d56d26d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-25 20:49:59 +0900

    [add] - 絵文字ピッカーの言語設定とクリックハンドラ作成

--- src/components/Channel/ChannelContent/HoverMenu/EmojiPicker.tsx
+++ src/components/Channel/ChannelContent/HoverMenu/EmojiPicker.tsx
@@ -1,16 +1,32 @@
 import "emoji-picker-element";
-import {onMount} from "solid-js";
+import {onCleanup, onMount} from "solid-js";
 import {Picker} from "emoji-picker-element";
+import {EmojiClickEvent} from "emoji-picker-element/shared";
+import ja from 'emoji-picker-element/i18n/ja';
 
 export default function EmojiPicker() {
-  const picker = new Picker();
+  const picker = new Picker({
+    locale: "ja",
+    i18n: ja
+  });
+
+  const emojiClickHandler = (event: EmojiClickEvent) => {
+    console.log("EmojiPicker :: onMount : クリックしたやつ->", event.detail);
+  }
 
   onMount(() => {
     console.log("EmojiPicker :: onMount : emoji-picker-element", Picker);
     const emojiPickerDiv = document.getElementById("emojiPickerDiv");
     if (emojiPickerDiv) {
       emojiPickerDiv.appendChild(picker);
+
+      //絵文字クリックしたときのハンドラリンク
+      picker.addEventListener('emoji-click', emojiClickHandler);
     }
+  });
+
+  onCleanup(() => {
+    picker.removeEventListener('emoji-click', emojiClickHandler);
   });
 
   return (
