99042c71007e5057f7a6a4f487e079c9a00c0852
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-26 10:14:28 +0900

    [change] - リアクションの表示に使う絵文字の処理を改善

--- src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions.tsx
+++ src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions.tsx
@@ -1,5 +1,5 @@
 import {IMessage} from "~/types/Message";
-import {For, onMount} from "solid-js";
+import {createEffect, createResource, For, on, onMount} from "solid-js";
 import {Card} from "~/components/ui/card";
 import { Database } from 'emoji-picker-element';
 import {createMutable} from "solid-js/store";
@@ -8,19 +8,26 @@ export default function RenderEmojiReactions(props: {reaction: IMessage["reactio
   const db = new Database();
   const emojiToRender:{ [key: string]: string } = createMutable({})
 
-  onMount(async () => {
-    //リアクションデータごとに絵文字をレンダーする
-    for (const r of props.reaction) {
-      //データ取得、無ければ停止
-      const emojiData = await db.getEmojiByShortcode(r.emojiCode);
-      if (emojiData === null) return;
+  //props.reactionの長さの変更を検知して表示に使う絵文字データを取得、格納する
+  createEffect(on(
+    ()=>props.reaction.length.toString(),
+    async () => {
+      console.log("RenderEmojiReactions :: createEffect : props.reaction.length->", props.reaction.length);
 
-      console.log("RenderEmojiReactions :: getEmoji : 結果->", emojiData);
-      //絵文字そのものを格納
-      // @ts-ignore - 参照は正常にできている
-      emojiToRender[r.emojiCode] = emojiData.unicode;
+      //リアクションデータごとに絵文字をレンダーする
+      for (const r of props.reaction) {
+        if (emojiToRender[r.emojiCode] !== undefined) continue;
+        //データ取得、無ければ停止
+        const emojiData = await db.getEmojiByShortcode(r.emojiCode);
+        if (emojiData === null) return;
+
+        console.log("RenderEmojiReactions :: getEmoji : 結果->", emojiData);
+        //絵文字そのものを格納
+        // @ts-ignore - 参照は正常にできている
+        emojiToRender[r.emojiCode] = emojiData.unicode;
+      }
     }
-  })
+  ));
 
   return (
     <div class={"py-1 flex items-center flex-wrap gap-1"}>
