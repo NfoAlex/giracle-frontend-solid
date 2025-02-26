e22a8ef0bbc0c5a432caecafa39546ea947f186a
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-26 12:13:08 +0900

    [add] - 初期処理でカスタム絵文字取得するように

--- src/stores/CustomEmoji.ts
+++ src/stores/CustomEmoji.ts
@@ -2,5 +2,5 @@ import {createStore} from "solid-js/store";
 import {ICustomEmoji} from "~/types/Message";
 import {Database} from "emoji-picker-element";
 
-const [storeCustomEmoji, setStoreCustomEmoji] = createStore<ICustomEmoji[]>([]);
+export const [storeCustomEmoji, setStoreCustomEmoji] = createStore<ICustomEmoji[]>([]);
 export const emojiDB = new Database();
