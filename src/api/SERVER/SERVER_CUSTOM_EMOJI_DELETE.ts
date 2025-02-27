9122eca247965007b10af77fcc4093029b71d25d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-28 03:11:25 +0900

    [add] - カスタム絵文字削除APIハンドラ作成

---
+++ src/api/SERVER/SERVER_CUSTOM_EMOJI_DELETE.ts
@@ -0,0 +1,19 @@
+import {ICustomEmoji} from "~/types/Message";
+
+export default async function DELETE_SERVER_CUSTOM_EMOJI_DELETE(
+  _emojiCode: string,
+): Promise<{
+  message: string;
+  data: ICustomEmoji[];
+}> {
+  const res = await fetch("/api/server/custom-emoji/delete", {
+    method: "DELETE",
+    body: JSON.stringify({
+      emojiCode: _emojiCode,
+    })
+  }).catch((err) => {
+    throw new Error("SERVER_CUSTOM_EMOJI_DELETE :: err->", err);
+  });
+  if (!res.ok) throw new Error(await res.text());
+  return res.json();
+}
