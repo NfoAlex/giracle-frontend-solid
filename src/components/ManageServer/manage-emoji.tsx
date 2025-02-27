7fbd70f1c0a603bc44b9b9d3102af61482fc8e89
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-02-28 02:23:00 +0900

    [add] - カスタム絵文字の管理表示をできるように

--- src/components/ManageServer/manage-emoji.tsx
+++ src/components/ManageServer/manage-emoji.tsx
@@ -1,8 +1,6 @@
 import {Card} from "~/components/ui/card";
-import {For} from "solid-js";
+import {For, Show} from "solid-js";
 import {storeCustomEmoji} from "~/stores/CustomEmoji";
-import {Button} from "~/components/ui/button";
-import {IconPlus} from "@tabler/icons-solidjs";
 import CreateCustomEmoji from "~/components/ManageServer/ManageEmoji/CreateCustomEmoji";
 
 export default function ManageEmoji() {
@@ -10,12 +8,18 @@ export default function ManageEmoji() {
     <div class="flex flex-col overflow-y-auto h-full gap-2">
 
       <Card class={"p-2 max-h-full overflow-y-auto"}>
+        <Show when={storeCustomEmoji.length === 0}>
+          <div class={"text-center py-5"}>
+            <p>カスタム絵文字がありません。</p>
+          </div>
+        </Show>
+
         <For each={storeCustomEmoji}>
           {
             (emoji) => (
               <div class={"flex items-center gap-2"}>
                 <p>{ emoji.code}</p>
-                <img src={"/api/server/custom-emoji/" + emoji.id} alt={emoji.code} class={"w-8 h-8"} />
+                <img src={"/api/server/custom-emoji/" + emoji.code} alt={emoji.code} class={"w-8 h-8"} />
               </div>
             )
           }
