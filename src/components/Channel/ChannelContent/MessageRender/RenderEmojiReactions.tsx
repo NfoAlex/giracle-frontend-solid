10024bd72b9ea7e2999bb91f7c2d9a1cc5841f1d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-03-04 16:06:40 +0900

    [change] - 絵文字レンダー部分を独立

--- src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions.tsx
+++ src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions.tsx
@@ -1,16 +1,11 @@
 import {IMessage} from "~/types/Message";
-import {createEffect, For, on, Show} from "solid-js";
+import {For} from "solid-js";
 import {Card} from "~/components/ui/card";
-import {createMutable} from "solid-js/store";
 import DELETE_MESSAGE_DELETE_EMOJI_REACTION from "~/api/MESSAGE/MESSAGE_DELETE_EMOJI_REACTION";
 import POST_MESSAGE_EMOJI_REACTION from "~/api/MESSAGE/MESSAGE_EMOJI_REACTION";
-import {emojiDB} from "~/stores/CustomEmoji";
+import RenderEmoji from "~/components/unique/RenderEmoji";
 
 export default function RenderEmojiReactions(props: {reaction: IMessage["reactionSummary"], messageId: string, channelId: string}) {
-
-  const emojiToRender:{ [key: string]: string } = createMutable({});
-  const urlToRender:{ [key: string]: string } = createMutable({});
-
   /**
    * リアクションを削除する
    * @param emojiCode
@@ -31,36 +26,6 @@ export default function RenderEmojiReactions(props: {reaction: IMessage["reactio
       });
   }
 
-  //props.reactionの長さの変更を検知して表示に使う絵文字データを取得、格納する
-  createEffect(on(
-    ()=>props.reaction.length.toString(),
-    async () => {
-      //console.log("RenderEmojiReactions :: createEffect : props.reaction.length->", props.reaction.length);
-
-      //リアクションデータごとに絵文字をレンダーする
-      for (const r of props.reaction) {
-        if (emojiToRender[r.emojiCode] !== undefined) continue;
-        //データ取得、無ければ停止
-        const emojiData = await emojiDB.getEmojiByShortcode(r.emojiCode);
-        if (emojiData === null) continue;
-
-        //console.log("RenderEmojiReactions :: createEffect : emojiData->", emojiData);
-        //カスタム絵文字だとURLがあるのでそれを使う
-        // @ts-ignore - 参照は正常にできている
-        if (emojiData.url !== undefined) {
-          //絵文字のURLを格納
-          // @ts-ignore - 参照は正常にできている
-          urlToRender[r.emojiCode] = emojiData.url;
-          continue;
-        }
-
-        //絵文字そのものを格納
-        // @ts-ignore - 参照は正常にできている
-        emojiToRender[r.emojiCode] = emojiData.unicode;
-      }
-    }
-  ));
-
   return (
     <div class={"py-1 flex items-center flex-wrap gap-1"}>
       <For each={props.reaction}>
@@ -71,15 +36,7 @@ export default function RenderEmojiReactions(props: {reaction: IMessage["reactio
                 onClick={() => r.includingYou ? deleteReaction(r.emojiCode) : addReaction(r.emojiCode)}
                 class={`p-1 text-sm flex items-center gap-1 cursor-pointer hover:bg-accent hover:border-background border-accent ${r.includingYou ? "bg-accent border-primary" : ""}`}
               >
-                <Show when={emojiToRender[r.emojiCode]!==undefined}>
-                  <span>{ emojiToRender[r.emojiCode]!==undefined ? emojiToRender[r.emojiCode] : r.emojiCode.slice(0,5) }</span>
-                </Show>
-                <Show when={urlToRender[r.emojiCode]!==undefined}>
-                  <img src={urlToRender[r.emojiCode]} alt={r.emojiCode} class={"w-5 h-5"} />
-                </Show>
-                <Show when={urlToRender[r.emojiCode]===undefined && emojiToRender[r.emojiCode]===undefined}>
-                  <span>{ r.emojiCode.length > 5 ? r.emojiCode.slice(0,5) + "..." : r.emojiCode }</span>
-                </Show>
+                <RenderEmoji emojiCode={r.emojiCode} />
                 <span>{ r.count }</span>
               </Card>
             )
