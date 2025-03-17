1462b939f25196ab0d5f7a9c2deec52958ee97d3
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-03-17 20:46:51 +0900

    [add] - URLプレビューで動画プレイヤーをモーダルで表示するように

--- src/components/Channel/ChannelContent/MessageRender/URLPreview.tsx
+++ src/components/Channel/ChannelContent/MessageRender/URLPreview.tsx
@@ -1,19 +1,34 @@
 import type {IMessageUrlPreview} from "~/types/Message";
 import {Card, CardContent, CardHeader, CardTitle} from "~/components/ui/card";
-import {For} from "solid-js";
+import {For, Show} from "solid-js";
 import ImageWithModal from "~/components/unique/ImageWithModal";
+import VideoPlayerModal from "./URLPreview/VideoPlayerModal";
+import { IconLink } from "@tabler/icons-solidjs";
 
 export default function URLPreview(props: { MessageUrlPreview: IMessageUrlPreview[] }) {
   return (
     <div class={"py-1 flex flex-col gap-1"}>
       <For each={props.MessageUrlPreview}>
         {(urlPreview: IMessageUrlPreview) => (
+
           <Card class={"flex flex-col md:w-72 lg:w-96 sm:w-1/2 w-full"}>
-            <div class={"border-b-2 grow"}>
+            <div class={"border-b-2 grow relative"}>
               <ImageWithModal class={"mx-auto rounded-t max-h-32 md:max-h-fit w-fit object-cover"} src={urlPreview.imageLink} />
+
+              <Show when={urlPreview.videoLink}>
+                <span class={"absolute bottom-3 right-3 shadow-xl"}>
+                  <VideoPlayerModal urlPreview={urlPreview} />
+                </span>
+              </Show>
             </div>
             <CardHeader class={"flex flex-row items-center gap-1"}>
-              {urlPreview.faviconLink && <img class={"w-4 h-fit shrink"} src={urlPreview.faviconLink} alt="favicon" />}
+            {
+              urlPreview.faviconLink
+              ?
+              <img class={"w-4 h-fit shrink"} src={urlPreview.faviconLink} alt="favicon" />
+              :
+              <IconLink />
+            }
               <CardTitle>
                 <a href={urlPreview.url} target="_blank" rel="noreferrer">
                   {urlPreview.title}
@@ -24,6 +39,7 @@ export default function URLPreview(props: { MessageUrlPreview: IMessageUrlPrevie
               { urlPreview.description }
             </CardContent>
           </Card>
+
         )}
       </For>
     </div>
---
+++ src/components/Channel/ChannelContent/MessageRender/URLPreview/VideoPlayerModal.tsx
@@ -0,0 +1,41 @@
+import { IconLink, IconPlayerPlayFilled } from "@tabler/icons-solidjs";
+import { Button } from "~/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import type { IMessageUrlPreview } from "~/types/Message";
+
+export default function VideoPlayerModal(props: { urlPreview: IMessageUrlPreview }) {
+
+  if (props.urlPreview.videoLink === null) return null;
+
+  return (
+    <Dialog>
+
+      <DialogTrigger>
+        <Button size="icon">
+          <IconPlayerPlayFilled />
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent class="width-fit height-fit p-0 pt-10 m-0 rounded-md">
+        <div class="flex items-center gap-2 px-6 truncate line-clamp-1">
+          {
+            props.urlPreview.faviconLink
+            ?
+            <img class={"w-4 h-fit shrink"} src={props.urlPreview.faviconLink} alt="favicon" />
+            :
+            <IconLink />
+          }
+          <p>{ props.urlPreview.title }</p>
+        </div>
+
+        <video
+          height={"auto"}
+          width={"auto"}
+          src={props.urlPreview.videoLink}
+          controls
+        />
+      </DialogContent>
+
+    </Dialog>
+  )
+}
