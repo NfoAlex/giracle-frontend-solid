c6223dc95aba4e986e98c0e1f3a992248dd5132b
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-03-20 23:13:27 +0900

    [add] - 絵文字リアクションした人を取得するハンドラ作成

---
+++ src/api/MESSAGE/MESSAGE_WHO_REACTED.ts
@@ -0,0 +1,18 @@
+export default async function GET_MESSAGE_WHO_REACTED(
+  _messageId: string,
+  _emojiCode: string
+): Promise<{
+  message: "Fetched reactions",
+  data: string[]
+}> {
+  const res = await fetch(`/api/message/who-reacted?messageId=${_messageId}&emojiCode=${_emojiCode}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).catch((err) => {
+    throw new Error("MESSAGE_WHO_REACTED :: err->", err);
+  });
+  if (!res.ok) throw new Error(await res.text());
+  return res.json();
+}
