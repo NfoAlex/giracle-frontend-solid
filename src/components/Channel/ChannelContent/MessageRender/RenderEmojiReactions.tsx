caa558ab370cff52a8eceb6eb8f8eeed7b1eb6ff
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-03-29 00:24:36 +0900

    [add] - ホバーでリアクションしたユーザーIdを一応表示できるように

--- src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions.tsx
+++ src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions.tsx
@@ -1,5 +1,5 @@
 import type {IMessage} from "~/types/Message";
-import {For} from "solid-js";
+import {createSignal, For, Show} from "solid-js";
 import {Card} from "~/components/ui/card";
 import DELETE_MESSAGE_DELETE_EMOJI_REACTION from "~/api/MESSAGE/MESSAGE_DELETE_EMOJI_REACTION";
 import POST_MESSAGE_EMOJI_REACTION from "~/api/MESSAGE/MESSAGE_EMOJI_REACTION";
@@ -11,6 +11,8 @@ export default function RenderEmojiReactions(props: {reaction: IMessage["reactio
   //リアクションをしているユーザー取得のためのデータJSONと取得状態用JSON
   const reactedUserArrs = createMutable<{ [emojiCode:string]: string[] }>({});
   const statusFetchingEmojiCode = createMutable<{ [emojiCode:string]: boolean }>({});
+  //ホバー状態に入っている絵文字コード
+  const [hoveringEmojiCode, setHoverEmojiCode] = createSignal<string>("");
 
   /**
    * リアクションを削除する
@@ -38,6 +40,8 @@ export default function RenderEmojiReactions(props: {reaction: IMessage["reactio
    * @param emojiCode
    */
   const fetchReactedUser = async (emojiCode: string) => {
+    //もしホバーをやめているなら停止
+    if (hoveringEmojiCode() !== emojiCode) return;
     //指定の絵文字リアクションをしているユーザーを取得中の場合は何もしない
     statusFetchingEmojiCode[emojiCode] = true;
     if (reactedUserArrs[emojiCode]) return;
@@ -58,11 +62,25 @@ export default function RenderEmojiReactions(props: {reaction: IMessage["reactio
             return (
               <Card
                 onClick={() => r.includingYou ? deleteReaction(r.emojiCode) : addReaction(r.emojiCode)}
-                onMouseOver={() => fetchReactedUser(r.emojiCode)}
-                class={`p-1 text-sm flex items-center gap-1 cursor-pointer hover:bg-accent hover:border-background border-accent ${r.includingYou ? "bg-accent border-primary" : ""}`}
+                onMouseOver={() =>
+                  setTimeout(()=>{ setHoverEmojiCode(r.emojiCode); fetchReactedUser(r.emojiCode); }, 500)
+                }
+                onMouseLeave={() => setHoverEmojiCode("")}
+                class={`relative p-1 text-sm flex items-center gap-1 cursor-pointer hover:bg-accent hover:border-background border-accent ${r.includingYou ? "bg-accent border-primary" : ""}`}
               >
                 <RenderEmoji emojiCode={r.emojiCode} />
                 <span>{ r.count }</span>
+
+                {/* ホバー表示 */}
+                <Show when={hoveringEmojiCode() === r.emojiCode}>
+                  <Card class="absolute bottom-full p-2 left-0">
+                    <code>{ r.emojiCode }</code>
+                    <hr />
+                    { reactedUserArrs[r.emojiCode] !== undefined &&
+                      <span>{ reactedUserArrs[r.emojiCode].join(",") }</span>
+                    }
+                  </Card>
+                </Show>
               </Card>
             )
           }
