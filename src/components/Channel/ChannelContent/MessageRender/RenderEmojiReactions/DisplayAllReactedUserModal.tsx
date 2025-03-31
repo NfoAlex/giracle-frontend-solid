51579bd9b621e94422c6feca9512206c0fda4471
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-03-31 22:21:13 +0900

    [add] - リアクションしたユーザーにアバター表示を追加

--- src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions/DisplayAllReactedUserModal.tsx
+++ src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions/DisplayAllReactedUserModal.tsx
@@ -1,5 +1,6 @@
 import { createEffect, createSignal, For, on, Show } from "solid-js";
 import GET_MESSAGE_WHO_REACTED from "~/api/MESSAGE/MESSAGE_WHO_REACTED";
+import { Avatar, AvatarImage } from "~/components/ui/avatar";
 import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "~/components/ui/dialog";
 import RenderEmoji from "~/components/unique/RenderEmoji";
 import { getterUserinfo } from "~/stores/Userinfo";
@@ -31,13 +32,22 @@ export default function DisplayAllReactedUserModal(props: { messageId: string, e
             <p>リアクションしたユーザー表示</p> <RenderEmoji emojiCode={props.emojiCode} />
           </span>
         </DialogHeader>
-        <span class="grow shrink">
+        
+        <hr />
+
+        <span class="grow shrink flex flex-col gap-2 overflow-y-auto">
           <Show when={props.onOpen}>
             <For each={reactedUserArrs()}>
               {(userId) => {
                 return (
-                  <div>
-                    { getterUserinfo(userId)?.name || "..." }
+                  <div class="px-2 flex items-center gap-2">
+                    <Avatar class="w-8 h-8">
+                      <AvatarImage
+                        src={"/api/user/icon/" + getterUserinfo(userId)?.id}
+                        alt={userId}
+                      />
+                    </Avatar>
+                    <p>{ getterUserinfo(userId)?.name || "..." }</p>
                   </div>
                 )
               }}
