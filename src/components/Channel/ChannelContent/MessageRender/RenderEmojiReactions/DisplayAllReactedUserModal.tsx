eb956e9e3ff87677ce4a1d7b46c074168e68e48c
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-03-31 21:41:11 +0900

    [add] - 一旦リアクションユーザー表示を拡大表示できるように

--- src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions/DisplayAllReactedUserModal.tsx
+++ src/components/Channel/ChannelContent/MessageRender/RenderEmojiReactions/DisplayAllReactedUserModal.tsx
@@ -1,12 +1,35 @@
+import { createEffect, createSignal, on, Show } from "solid-js";
+import GET_MESSAGE_WHO_REACTED from "~/api/MESSAGE/MESSAGE_WHO_REACTED";
 import { Button } from "~/components/ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "~/components/ui/dialog";
 
 // ToDo :: 指定した人数しかとれない
 export default function DisplayAllReactedUserModal(props: { messageId: string, emojiCode: string, onOpen: boolean, onOpenChange: (open: boolean) => void }) {
+  const [reactedUserArrs, setReactedUserArrs] = createSignal<string[]>([]);
+  
+  /**
+   * propsからの絵文字コード指定の更新を監視してデータ取得
+   */
+  createEffect(on(
+    () => props.emojiCode,
+    (emojiCode) => {
+      if (props.emojiCode === "") return;
+      GET_MESSAGE_WHO_REACTED(props.messageId, emojiCode)
+        .then((res) => {
+          setReactedUserArrs(res.data);
+        })
+        .catch((e) => console.error("DisplayAllReactedUserModal :: createEffect : e->", e))
+    }
+  ));
+  
   return (
     <Dialog onOpenChange={props.onOpenChange} open={props.onOpen}>
       <DialogContent>
-        <p>ここで人表示</p>
+        <DialogHeader>リアクションしたユーザー</DialogHeader>
+        <Show when={props.onOpen}>
+          <p>ここで人表示 : { props.emojiCode }</p>
+          { reactedUserArrs().join(",") }
+        </Show>
       </DialogContent>
     </Dialog>
   )
