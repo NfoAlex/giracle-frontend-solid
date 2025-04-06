6ca2115a7117da4e6925cd876d361f615c1a913e
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-04-06 17:51:21 +0900

    [add] - 設定ページの土台作成

---
+++ src/routes/config.tsx
@@ -0,0 +1,14 @@
+export default function Config() {
+  return (
+    <div class="flex flex-col md:flex-row">
+      config
+      <div class="w-full md:w-64 h-32 md:h-full">
+        ここでサイドバー
+      </div>
+
+      <div>
+        ここで設定ページ
+      </div>
+    </div>
+  )
+}
