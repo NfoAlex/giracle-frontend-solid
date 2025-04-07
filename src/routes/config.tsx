482270a705ce85e4084c051e3cfab5cf00c412ae
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-04-07 17:54:18 +0900

    [change] - 設定バー表示の調整

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -1,12 +1,13 @@
 import { useParams } from "@solidjs/router";
+import { IconBell, IconEye, IconHash, IconUser } from "@tabler/icons-solidjs";
 import { createSignal } from "solid-js";
 import ConfigChat from "~/components/Config/ConfigChat";
 import ConfigDisplay from "~/components/Config/ConfigDisplay";
 import ConfigNotification from "~/components/Config/ConfigNotification";
 import ConfigProfile from "~/components/Config/ConfigProfile";
-import { Button } from "~/components/ui/button";
 import { Card } from "~/components/ui/card";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
+import { SidebarMenuButton } from "~/components/ui/sidebar";
 import SidebarTriggerWithDot from "~/components/unique/SidebarTriggerWithDot";
 
 export default function Config() {
@@ -60,23 +61,39 @@ export default function Config() {
         </div>
           
         {/* デスクトップ用設定ページ選択 */}
-        <Card class="shrink-0 p-2 h-full w-64 hidden md:flex flex-col gap-1 overflow-y-auto text-left">
-          <Button
+        <Card class="shrink-0 p-1 h-full w-64 hidden md:flex flex-col gap-1 overflow-y-auto text-left">
+          <SidebarMenuButton
             onClick={()=>setDisplayMode("profile")}
-            variant={displayMode()==="profile" ? "secondary" : "ghost"}
-          >プロフィール</Button>
-          <Button
+            variant={displayMode()==="profile"?"outline":"default"}
+            size={"lg"}
+          >
+            <IconUser />
+            プロフィール
+          </SidebarMenuButton>
+          <SidebarMenuButton
             onClick={()=>setDisplayMode("chat")}
-            variant={displayMode()==="chat" ? "secondary" : "ghost"}
-          >会話</Button>
-          <Button
+            variant={displayMode()==="chat"?"outline":"default"}
+            size={"lg"}
+          >
+            <IconHash />
+            会話
+          </SidebarMenuButton>
+          <SidebarMenuButton
             onClick={()=>setDisplayMode("notification")}
-            variant={displayMode()==="notification" ? "secondary" : "ghost"}
-          >通知</Button>
-          <Button
+            variant={displayMode()==="notification"?"outline":"default"}
+            size={"lg"}
+          >
+            <IconBell />
+            通知
+          </SidebarMenuButton>
+          <SidebarMenuButton
             onClick={()=>setDisplayMode("display")}
-            variant={displayMode()==="display" ? "secondary" : "ghost"}
-          >表示</Button>
+            variant={displayMode()==="display"?"outline":"default"}
+            size={"lg"}
+          >
+            <IconEye />
+            表示
+          </SidebarMenuButton>
         </Card>
 
         <div class="overflow-y-auto grow md:px-2 md:pt-0 pt-4">
