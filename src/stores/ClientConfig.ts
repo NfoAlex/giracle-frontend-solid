807fc1a21267306628e5466264692be7f60349c8
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-04-07 17:28:31 +0900

    [add] - Ctrlキー送信のための設定項目追加

--- src/stores/ClientConfig.ts
+++ src/stores/ClientConfig.ts
@@ -1,6 +1,9 @@
 import { createMutable } from "solid-js/store";
 
 export const storeClientConfig = createMutable({
+  chat: {
+    sendWithCtrlKey: false
+  },
   notification: {
     notifyInbox: true,
     notifyAll: false
