afd85fe6c9b4c2810635cfe74750e652933aded1
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-04-07 16:54:17 +0900

    [add] - 通知設定コンポの土台作成

--- src/components/Config/Notification.tsx
+++ src/components/Config/Notification.tsx
@@ -1,7 +1,48 @@
+import { IconBell } from "@tabler/icons-solidjs";
+import { Card } from "../ui/card";
+
 export default function Notification() {
   return (
-    <div>
-      通知設定
+    <div class="flex flex-col gap-6">
+      
+      {/* 通知の有効化 */}
+      <Card class="p-4 flex flex-col gap-4">
+        <span class="flex items-center">
+          <span class="font-bold flex items-center gap-2">
+            <IconBell />
+            通知を有効化
+          </span>
+          <div class={"ml-auto flex items-center gap-2"}>
+            asdf
+          </div>
+        </span>
+
+        <hr />
+
+        <div>
+          Giracleによる通知を有効/無効を設定します。
+        </div>
+      </Card>
+
+      {/* すべてのメッセージ受信による通知の有効化 */}
+      <Card class="p-4 flex flex-col gap-4">
+        <span class="flex items-center">
+          <span class="font-bold flex items-center gap-2">
+            <IconBell />
+            全てのメッセージ通知を有効化
+          </span>
+          <div class={"ml-auto flex items-center gap-2"}>
+            asdf
+          </div>
+        </span>
+
+        <hr />
+
+        <div>
+          メンションにかかわらず、すべてのメッセージを受信した際に通知します。
+        </div>
+      </Card>
+
     </div>
   )
 }
