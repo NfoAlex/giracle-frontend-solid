9489ec0577e4df2f1af3ab43c2e6e587112bc163
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2025-05-14 18:02:06 +0900

    [add] - レイアウトの土台作成

--- src/components/Channel/ChannelHeader/ChannelManage/ChannelMembers/InviteUserModal.tsx
+++ src/components/Channel/ChannelHeader/ChannelManage/ChannelMembers/InviteUserModal.tsx
@@ -1,7 +1,11 @@
+import { createSignal } from "solid-js";
 import { Button } from "~/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import { TextField, TextFieldInput } from "~/components/ui/text-field";
 
 export default function InviteUserModal(props: { channelId: string }) {
+  const [searchQuery, setSearchQuery] = createSignal("");
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -9,7 +13,17 @@ export default function InviteUserModal(props: { channelId: string }) {
       </DialogTrigger>
 
       <DialogContent>
+        <TextField>
+          <TextFieldInput
+            value={searchQuery()}
+            onInput={setSearchQuery}
+            placeholder="招待するユーザー名を検索"
+          />
 
+          <hr class={"my-4"} />
+
+          <div>ここで結果表示</div>
+        </TextField>
       </DialogContent>
     </Dialog>
   );
