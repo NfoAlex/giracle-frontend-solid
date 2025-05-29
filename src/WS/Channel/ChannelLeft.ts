740c501fbe2c6ef5bb4e782cb1599133e11a0610
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-05-29 14:07:32 +0900

    [add] - チャンネル退出時に履歴と通知Storeを削除するように

--- src/WS/Channel/ChannelLeft.ts
+++ src/WS/Channel/ChannelLeft.ts
@@ -1,9 +1,30 @@
+import { setStoreHasNewMessage, storeHasNewMessage } from "~/stores/HasNewMessage";
+import { setStoreHistory, storeHistory } from "~/stores/History";
 import { setStoreMyUserinfo, storeMyUserinfo } from "~/stores/MyUserinfo";
 
 export default function WSChannelLeft(dat: { channelId: string }) {
   console.log("WSChannelLeft :: triggered dat->", dat);
 
   const myJoinedChannel = storeMyUserinfo.ChannelJoin;
+
+  //履歴Storeから対象チャンネル分を削除
+  if (storeHistory[dat.channelId] !== undefined) {
+    setStoreHistory((prev) => {
+      const newHistory = { ...prev };
+      delete newHistory[dat.channelId];
+      return newHistory;
+    });
+  }
+  //未読メッセージ通知Storeから対象チャンネル分を削除
+  if (storeHasNewMessage[dat.channelId] !== undefined) {
+    setStoreHasNewMessage((prev) => {
+      const newHasNewMessage = { ...prev };
+      delete newHasNewMessage[dat.channelId];
+      return {
+        ...newHasNewMessage
+      };
+    });
+  }
   
   //チャンネル参加しているなら削除
   if (myJoinedChannel.map(cj => cj.channelId).includes(dat.channelId)) {
