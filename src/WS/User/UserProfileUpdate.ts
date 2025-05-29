fefa805618574c66ca9408e2333e1ed2a3b640ba
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-05-29 13:27:22 +0900

    [add] - 自分のユーザー情報が更新されたときのStore更新

--- src/WS/User/UserProfileUpdate.ts
+++ src/WS/User/UserProfileUpdate.ts
@@ -1,8 +1,20 @@
 import type {IUser} from "~/types/User";
 import {updateUserinfo} from "~/stores/Userinfo";
+import { setStoreMyUserinfo, storeMyUserinfo } from "~/stores/MyUserinfo";
 
 export default function WSUserProfileUpdate(dat: IUser) {
   console.log("WSProfileUpdate :: triggered dat->", dat);
+
+  //自分のユーザー情報が更新された場合は、MyUserinfoを更新
+  if (dat.id === storeMyUserinfo.id) {
+    //自分の情報が更新された場合は、MyUserinfoを更新
+    setStoreMyUserinfo((u) => {
+      return {
+        ...u,
+        ...dat
+      };
+    });
+  }
 
   //ユーザー情報を格納
   updateUserinfo(dat);
