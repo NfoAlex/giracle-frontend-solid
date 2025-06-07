fd37216b7fbe2c233eefb1c7542a14e2369503b4
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-06-07 20:15:52 +0900

    [change] - レイアウト調整

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -29,37 +29,37 @@ export default function Search() {
         <p>メッセージ検索</p>
       </Card>
       
-      <div class="lg:w-4/5 mx-auto w-full h-full mt-2 flex flex-col gap-2">
-        <span class="w-full flex items-center gap-2">
-          <TextField class="grow">
-            <TextFieldInput
-              value={query()}
-              onChange={(e) => setQuery(e.currentTarget.value)}
-            ></TextFieldInput>
-          </TextField>
+      <span class="lg:w-4/5 mx-auto w-full flex items-center gap-2 mt-2">
+        <TextField class="grow">
+          <TextFieldInput
+            value={query()}
+            onChange={(e) => setQuery(e.currentTarget.value)}
+          ></TextFieldInput>
+        </TextField>
 
-          <Button onClick={searchIt} size={"icon"}><IconSearch /></Button>
+        <Button onClick={searchIt} size={"icon"}><IconSearch /></Button>
+      </span>
+
+      <hr class="lg:w-4/5 mx-auto mt-2" />
+
+      <div class="lg:w-4/5 mx-auto grow overflow-y-auto py-4">
+        <span class="flex flex-col gap-2 overflow-y-auto">
+          <For each={searchResults()}>
+            {(message) => message.userId !== "SYSTEM" && (
+              <Card class="p-2">
+                <Avatar>
+                  <AvatarImage src={"/api/user/icon/" + message.userId} />
+                  <AvatarFallback>
+                    { getterUserinfo(message.userId).name.slice(0,1) }
+                  </AvatarFallback>
+                </Avatar>
+                <span class={"grow"}>
+                  <MessageRender message={message} displayUserName={true} />
+                </span>
+              </Card>
+            )}
+          </For>
         </span>
-
-        <div class="grow overflow-y-auto">
-          <span class="flex flex-col gap-2 overflow-y-auto">
-            <For each={searchResults()}>
-              {(message) => message.userId !== "SYSTEM" && (
-                <Card class="p-2">
-                  <Avatar>
-                    <AvatarImage src={"/api/user/icon/" + message.userId} />
-                    <AvatarFallback>
-                      { getterUserinfo(message.userId).name.slice(0,1) }
-                    </AvatarFallback>
-                  </Avatar>
-                  <span class={"grow"}>
-                    <MessageRender message={message} displayUserName={true} />
-                  </span>
-                </Card>
-              )}
-            </For>
-          </span>
-        </div>
       </div>
     </div>
   );
