5bad70f1553fffc542bf5f08ed1776d7fbb23c4f
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-06-07 22:09:34 +0900

    [change] - メッセージのユーザー名表示と検索入力部分のスタイル調整

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -7,6 +7,7 @@ import { Button } from "~/components/ui/button";
 import { Card } from "~/components/ui/card";
 import { TextField, TextFieldInput } from "~/components/ui/text-field";
 import SidebarTriggerWithDot from "~/components/unique/SidebarTriggerWithDot";
+import UserinfoModalWrapper from "~/components/unique/UserinfoModalWrapper";
 import { getterUserinfo } from "~/stores/Userinfo";
 import type { IMessage } from "~/types/Message";
 
@@ -32,35 +33,38 @@ export default function Search() {
         <p>メッセージ検索</p>
       </Card>
       
-      <span class="lg:w-4/5 mx-auto w-full flex items-center gap-2 mt-2">
+      <span class="mx-auto w-full flex items-center gap-2 mt-2">
         <TextField class="grow">
           <TextFieldInput
             value={query()}
             placeholder="メッセージ文を検索"
             onChange={(e) => setQuery(e.currentTarget.value)}
+            class="h-12 md:h-10"
           ></TextFieldInput>
         </TextField>
 
-        <Button onClick={searchIt} size={"icon"}><IconSearch /></Button>
+        <Button onClick={searchIt} size={"icon"} class="md:w-10 md:h-10 h-12 w-12"><IconSearch /></Button>
       </span>
 
-      <hr class="lg:w-4/5 mx-auto mt-2" />
+      <hr class=" mt-2" />
 
-      <div class="lg:w-4/5 mx-auto grow overflow-y-auto py-4">
-        <span class="flex flex-col gap-2 overflow-y-auto">
+      <div class="w-full grow overflow-y-auto pt-2 pb-4">
+        <span class="flex flex-col gap-6 md:gap-2 overflow-y-auto overflow-x-hidden">
           { !searchedOnce() && <p class="text-center">メッセージ文を検索してください。</p> }
           { searchedOnce() && searchResults().length === 0 && <p class="text-center">結果が見つかりませんでした...</p>}
           <For each={searchResults()}>
             {(message) => message.userId !== "SYSTEM" && (
               <Card class="p-2">
-                <Avatar>
-                  <AvatarImage src={"/api/user/icon/" + message.userId} />
-                  <AvatarFallback>
-                    { getterUserinfo(message.userId).name.slice(0,1) }
-                  </AvatarFallback>
-                </Avatar>
+                <span>
+                  <UserinfoModalWrapper userId={message.userId} class="flex flex-row iterms-center gap-2">
+                    <Avatar>
+                      <AvatarImage src={"/api/user/icon/" + message.userId} />
+                    </Avatar>
+                    <p style="padding-top: 0.45rem;">{ getterUserinfo(message.userId).name }</p>
+                  </UserinfoModalWrapper>
+                </span>
                 <span class={"grow"}>
-                  <MessageRender message={message} displayUserName={true} />
+                  <MessageRender message={message} displayUserName={false} />
                 </span>
               </Card>
             )}
