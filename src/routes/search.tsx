5369c7e0e24dd0f6839cc13d98ed28bb3e40b5bb
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-06-07 22:21:48 +0900

    [add] - もっと読み込むボタン追加

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -15,15 +15,28 @@ export default function Search() {
   const [query, setQuery] = createSignal("");
   const [searchResults, setSearchResults] = createSignal<IMessage[]>([]);
   const [searchedOnce, setSearchedOnce] = createSignal(false);
+  const [loadIndex, setLoadIndex] = createSignal(1);
+  const [processing, setProcessing] = createSignal(false);
 
-  const searchIt = () => {
-    GET_MESSAGE_SEARCH({ _content: query() })
+  const searchIt = (insertMode: boolean = false) => {
+    setProcessing(true);
+    GET_MESSAGE_SEARCH({ _content: query(), _loadIndex: loadIndex() + (insertMode ? 1 : 0)})
       .then((r) => {
+        //システムメッセージを除外
+        const result = r.data.filter((msg) => msg.userId !== "SYSTEM");
+
         console.log("Search :: search :: r ->", r);
-        setSearchResults(r.data);
+        //挿入かどうかで処理を分ける
+        if (insertMode) {
+          setSearchResults((prev) => [...prev, ...result]);
+        } else {
+          setLoadIndex(1); //リセット
+          setSearchResults(result);
+        }
         setSearchedOnce(true);
       })
-      .catch((e) => console.error("Search :: search :: e ->", e));
+      .catch((e) => console.error("Search :: search :: e ->", e))
+      .finally(() => setProcessing(false));
   }
 
   return (
@@ -43,7 +56,12 @@ export default function Search() {
           ></TextFieldInput>
         </TextField>
 
-        <Button onClick={searchIt} size={"icon"} class="md:w-10 md:h-10 h-12 w-12"><IconSearch /></Button>
+        <Button
+          onClick={() => searchIt(false)}
+          size={"icon"}
+          class="md:w-10 md:h-10 h-12 w-12"
+          disabled={processing()}
+        ><IconSearch /></Button>
       </span>
 
       <hr class=" mt-2" />
@@ -53,7 +71,7 @@ export default function Search() {
           { !searchedOnce() && <p class="text-center">メッセージ文を検索してください。</p> }
           { searchedOnce() && searchResults().length === 0 && <p class="text-center">結果が見つかりませんでした...</p>}
           <For each={searchResults()}>
-            {(message) => message.userId !== "SYSTEM" && (
+            {(message) => (
               <Card class="p-2">
                 <span>
                   <UserinfoModalWrapper userId={message.userId} class="flex flex-row iterms-center gap-2">
@@ -69,6 +87,12 @@ export default function Search() {
               </Card>
             )}
           </For>
+
+          {
+            searchResults().length >= 30
+            &&
+            <Button onClick={()=>searchIt(true)} variant={"secondary"}>もっと読み込む</Button>
+          }
         </span>
       </div>
     </div>
