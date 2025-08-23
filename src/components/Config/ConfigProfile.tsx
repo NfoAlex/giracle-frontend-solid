1fbe9c989a7c63fe6a19807220cf33d233227335
NfoAlex <vashro.alex1@protonmail.com>
2025-08-23 21:18:13 +0900

    [change] - プロフィールページのレイアウト変更とロール表示追加

--- src/components/Config/ConfigProfile.tsx
+++ src/components/Config/ConfigProfile.tsx
@@ -1,5 +1,5 @@
 import {IconCheck, IconCircleX, IconPencil} from "@tabler/icons-solidjs";
-import { Show, createSignal } from "solid-js";
+import { For, Show, createSignal } from "solid-js";
 import POST_USER_PROFILE_UPDATE from "~/api/USER/USER_PROFILE_UPDATE";
 import ChangeIcon from "~/components/Profile/ChangeIcon";
 import { Avatar, AvatarImage } from "~/components/ui/avatar";
@@ -7,8 +7,10 @@ import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardHeader } from "~/components/ui/card";
 import { Label } from "~/components/ui/label";
 import { TextField, TextFieldInput } from "~/components/ui/text-field";
-import { setStoreMyUserinfo, storeMyUserinfo } from "~/stores/MyUserinfo";
+import { getRolePower, setStoreMyUserinfo, storeMyUserinfo } from "~/stores/MyUserinfo";
 import ChangeBanner from "~/components/Profile/ChangeBanner";
+import RoleChip from "../unique/RoleChip";
+import POST_ROLE_UNLINK from "~/api/ROLE/ROLE_UNLINK";
 
 export default function ConfigProfile() {
   const [nameEditMode, setNameEditMode] = createSignal(false);
@@ -58,124 +60,154 @@ export default function ConfigProfile() {
       });
   };
 
+  /**
+     * ロールを解除
+     */
+  const unlinkRole = (roleId: string) => {
+    POST_ROLE_UNLINK(storeMyUserinfo.id, roleId)
+      .then(() => {
+        //console.log("RoleChip :: unlinkRole :: r ->", r);
+      })
+      .catch((e) => console.error("ConfigProfile :: unlinkRole :: err ->", e));
+  }
+
   return (
-    <div class="md:max-w-[450px] w-11/12 h-full mx-auto flex flex-col gap-2">
+    <div class="md:max-w-[750px] w-11/12 h-full overflow-y-auto mx-auto flex flex-col gap-2">
       <span class="flex items-center gap-2">
         <p class="font-bold text-2xl my-2">プロフィール</p>
       </span>
       <Card class="w-full">
         <CardHeader>
-          <div class={"relative mb-[37.5px]"}>
+          <div class={"relative"}>
             <img alt={"バナー画像"} src={"/api/user/banner/" + storeMyUserinfo.id} class="w-full rounded" />
-            <span class={"absolute top-2 right-2 z-50"}>
+            <span class={"absolute bottom-2 right-2 z-50"}>
               <ChangeBanner />
             </span>
-
-            <Avatar
-              class="mx-auto h-[75px] w-[75px] absolute z-50"
-              style={"left:calc(50% - 37.5px); right:left:calc(50% - 37.5px); bottom:-37.5px;"}
-            >
-              <AvatarImage src={`/api/user/icon/${storeMyUserinfo.id}`}/>
-            </Avatar>
           </div>
         </CardHeader>
-        
-        <div class="mx-auto w-fit">
-          <ChangeIcon/>
-        </div>
 
-        <CardContent class="mt-10">
-          <div class="w-full mx-auto">
+        <CardContent>
+
+          <div class="w-full mx-auto flex flex-col gap-4">
+            <span class="w-full flex md:flex-row flex-col gap-2 items-center">
+              <Avatar
+                class="w-16 h-16 z-50 mx-auto md:mx-0"
+              >
+                <AvatarImage src={`/api/user/icon/${storeMyUserinfo.id}`}/>
+              </Avatar>
+              <span class="md:ml-auto md:mr-0 mx-auto">
+                <ChangeIcon />
+              </span>
+            </span>
+
+            <hr />
+
             {/* 名前部分 */}
+            <span>
             <Label>名前</Label>
-            <Show
-              when={nameEditMode()}
-              fallback={
-                <div class="flex flex-row items-center gap-2">
-                  <p class="grow truncate">{storeMyUserinfo.name}</p>
-                  <Button onClick={() => setNameEditMode(!nameEditMode())} size={"icon"}>
-                    <IconPencil stroke="2"/>
-                  </Button>
+              <Show
+                when={nameEditMode()}
+                fallback={
+                  <div class="flex flex-row items-center gap-2">
+                    <p class="grow truncate">{storeMyUserinfo.name}</p>
+                    <Button onClick={() => setNameEditMode(!nameEditMode())} size={"icon"}>
+                      <IconPencil stroke="2"/>
+                    </Button>
+                  </div>
+                }
+              >
+                <div class="flex md:flex-row flex-col gap-2">
+                  <TextField class="grow">
+                    <TextFieldInput
+                      type="text"
+                      placeholder="新しいユーザー名"
+                      value={newName()}
+                      onInput={(e) => setNewName(e.currentTarget.value)}
+                    />
+                  </TextField>
+                  <div class="flex flex-row items-center gap-1">
+                    <Button onClick={changeName} variant={"ghost"} size={"icon"}>
+                      <IconCheck stroke="2"/>
+                    </Button>
+                    <Button
+                      onClick={() => setNameEditMode(!nameEditMode())}
+                      variant={"ghost"}
+                      size={"icon"}
+                    >
+                      <IconCircleX stroke="2"/>
+                    </Button>
+                  </div>
                 </div>
-              }
-            >
-              <div class="flex md:flex-row flex-col gap-2">
-                <TextField class="grow">
-                  <TextFieldInput
-                    type="text"
-                    placeholder="新しいユーザー名"
-                    value={newName()}
-                    onInput={(e) => setNewName(e.currentTarget.value)}
-                  />
-                </TextField>
-                <div class="flex flex-row items-center gap-1">
-                  <Button onClick={changeName} variant={"ghost"} size={"icon"}>
-                    <IconCheck stroke="2"/>
-                  </Button>
-                  <Button
-                    onClick={() => setNameEditMode(!nameEditMode())}
-                    variant={"ghost"}
-                    size={"icon"}
-                  >
-                    <IconCircleX stroke="2"/>
-                  </Button>
-                </div>
-              </div>
-            </Show>
+              </Show>
+            </span>
 
-            <hr class="my-3"/>
+            <hr />
 
             {/* 自己紹介部分 */}
-            <Label>自己紹介</Label>
-            <Show
-              when={selfIntroEditMode()}
-              fallback={
-                <div class="flex flex-row items-center gap-2">
-                  <p class="grow truncate">
-                    {storeMyUserinfo.selfIntroduction}
-                  </p>
-                  <Button
-                    onClick={() => setSelfIntroEditMode(!selfIntroEditMode())}
-                    size={"icon"}
-                  >
-                    <IconPencil stroke="2"/>
-                  </Button>
+            <span>
+              <Label>自己紹介</Label>
+              <Show
+                when={selfIntroEditMode()}
+                fallback={
+                  <div class="flex flex-row items-center gap-2">
+                    <p class="grow truncate">
+                      {storeMyUserinfo.selfIntroduction}
+                    </p>
+                    <Button
+                      onClick={() => setSelfIntroEditMode(!selfIntroEditMode())}
+                      size={"icon"}
+                    >
+                      <IconPencil stroke="2"/>
+                    </Button>
+                  </div>
+                }
+              >
+                <div class="flex md:flex-row flex-col gap-2">
+                  <TextField class="grow">
+                    <TextFieldInput
+                      type="text"
+                      placeholder="新しい自己紹介文"
+                      value={newSelfIntro()}
+                      onInput={(e) => setNewSelfIntro(e.currentTarget.value)}
+                    />
+                  </TextField>
+                  <div class="flex flex-row items-center gap-1">
+                    <Button
+                      onClick={changeSelfIntro}
+                      variant={"ghost"}
+                      size={"icon"}
+                    >
+                      <IconCheck stroke="2"/>
+                    </Button>
+                    <Button
+                      onClick={() => setSelfIntroEditMode(!selfIntroEditMode())}
+                      variant={"ghost"}
+                      size={"icon"}
+                    >
+                      <IconCircleX stroke="2"/>
+                    </Button>
+                  </div>
                 </div>
-              }
-            >
-              <div class="flex md:flex-row flex-col gap-2">
-                <TextField class="grow">
-                  <TextFieldInput
-                    type="text"
-                    placeholder="新しい自己紹介文"
-                    value={newSelfIntro()}
-                    onInput={(e) => setNewSelfIntro(e.currentTarget.value)}
-                  />
-                </TextField>
-                <div class="flex flex-row items-center gap-1">
-                  <Button
-                    onClick={changeSelfIntro}
-                    variant={"ghost"}
-                    size={"icon"}
-                  >
-                    <IconCheck stroke="2"/>
-                  </Button>
-                  <Button
-                    onClick={() => setSelfIntroEditMode(!selfIntroEditMode())}
-                    variant={"ghost"}
-                    size={"icon"}
-                  >
-                    <IconCircleX stroke="2"/>
-                  </Button>
-                </div>
-              </div>
-            </Show>
+              </Show>
+            </span>
 
-            <hr class="my-3"/>
+            <hr />
 
             {/* ロール一覧 */}
-            <Label>ロール</Label>
-            <p>Coming soon...</p>
+            <span>
+              <Label>ロール</Label>
+              <div class="flex flex-wrap gap-1 mt-2">
+                <For each={storeMyUserinfo.RoleLink}>
+                  {(role) =>
+                    <RoleChip
+                      deletable={getRolePower("manageRole")}
+                      roleId={role.roleId}
+                      onDelete={(roleId) => unlinkRole(roleId)}
+                    />
+                  }
+                </For>
+              </div>
+            </span>
           </div>
         </CardContent>
       </Card>
