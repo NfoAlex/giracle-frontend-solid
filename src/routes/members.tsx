aa713b55ef05fa7f3327c03b8b49c9052f736a87
NfoAlex <vashro.alex1@protonmail.com>
2026-01-30 23:08:05 +0900

    add - データ表示整備

--- src/routes/members.tsx
+++ src/routes/members.tsx
@@ -2,8 +2,12 @@ import type { IUser } from "~/types/User.ts";
 import { Table,TableBody,TableCaption,TableCell,TableHead,TableHeader,TableRow } from "~/components/ui/table.tsx";
 import { Card } from "~/components/ui/card.tsx";
 import SidebarTriggerWithDot from "~/components/unique/SidebarTriggerWithDot.tsx";
+import { Avatar,AvatarFallback,AvatarImage } from "~/components/ui/avatar.tsx";
+import { createSignal,For } from "solid-js";
 
 export default function Members() {
+  const [users, setUsers] = createSignal<IUser[]>([]);
+
   return (
     <div class="h-full p-2 flex flex-col gap-2">
       <Card class="w-full py-3 px-5 flex items-center gap-2">
@@ -21,12 +25,23 @@ export default function Members() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell class="font-medium">aikon</TableCell>
-            <TableCell class="font-medium">Member</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell class="text-right">$250.00</TableCell>
-          </TableRow>
+          <For each={users()}>
+            {(user) => (
+              <TableRow>
+                <TableCell class="font-medium">
+                  <Avatar class="mx-auto w-9 h-9">
+                    <AvatarImage src={`/api/user/icon/${"asdf"}`} />
+                    <AvatarFallback class="w-full h-full">{"asdf"}</AvatarFallback>
+                  </Avatar>
+                </TableCell>
+                <TableCell class="font-medium">{user.name}</TableCell>
+                <TableCell>{ user.isBanned.toString() }</TableCell>
+                <TableCell class="text-right">
+                  { user.RoleLink.join(", ") }
+                </TableCell>
+              </TableRow>
+            )}
+          </For>
         </TableBody>
       </Table>
     </div>
