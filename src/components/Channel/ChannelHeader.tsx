da2c1215b9ed1085efae0c3894782fbe30e424e9
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2026-02-27 19:48:42 +0900

    change - チャンネルヘッダにチャンネル取得状況表示を適応

--- src/components/Channel/ChannelHeader.tsx
+++ src/components/Channel/ChannelHeader.tsx
@@ -1,15 +1,15 @@
 import { useParams } from "@solidjs/router";
-import {createEffect, createSignal, Show} from "solid-js";
-import {IconLock} from "@tabler/icons-solidjs";
+import { createEffect, createSignal, Show, Switch, Match } from "solid-js";
+import { IconLock } from "@tabler/icons-solidjs";
 import SidebarTriggerWithDot from "../unique/SidebarTriggerWithDot.tsx";
 import { Card } from "../ui/card.tsx";
-import { directGetterChannelInfo } from "~/stores/ChannelInfo.ts";
+import { directGetterChannelInfo, storeChannelFetchStatus } from "~/stores/ChannelInfo.ts";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "../ui/hover-card.tsx";
 import ChannelManage from "./ChannelHeader/ChannelManage.tsx";
 
 export default function ChannelHeader() {
   const params = useParams();
-  const [currentChannelId, setCurrentChannelId] = createSignal<string>(params.id ?? "");
+  const [currentChannelId, setCurrentChannelId] = createSignal<string>(params.channelId ?? params.id ?? "");
 
   createEffect(() => {
     if (params.channelId !== undefined) {
@@ -18,32 +18,54 @@ export default function ChannelHeader() {
   });
 
   return (
-    <Card class="py-3 px-5 flex items-center w-full gap-2">
-      <SidebarTriggerWithDot />
+    <Switch>
+      <Match when={storeChannelFetchStatus[currentChannelId()] === "LOADING"}>
+        <Card class="py-3 px-5 flex items-center w-full gap-2">
+          <SidebarTriggerWithDot />
+          <span class={"shrink line-clamp-1"}>
+            <p>ロード中...</p>
+          </span>
+        </Card>
+      </Match>
 
-      {/* チャンネルの閲覧権限がある時の錠前アイコン */}
-      <Show when={directGetterChannelInfo(currentChannelId()).ChannelViewableRole.length !== 0}>
-        <HoverCard>
-          <HoverCardTrigger>
-            <IconLock class={"shrink-0 cursor-help"} size={"18"} />
-          </HoverCardTrigger>
-          <HoverCardContent>
-            ロールによる閲覧制限がかかっているチャンネルです
-          </HoverCardContent>
-        </HoverCard>
-      </Show>
+      <Match when={storeChannelFetchStatus[currentChannelId()] === "AVAILABLE"}>
+        <Card class="py-3 px-5 flex items-center w-full gap-2">
+          <SidebarTriggerWithDot />
 
-      <span class={"shrink line-clamp-1"}>
-        <p>{ directGetterChannelInfo(currentChannelId()).name }</p>
-      </span>
-      <p class="text-gray-400 mx-1"> | </p>
-      <span class={"shrink-[2] grow-0 line-clamp-1 max-w-[50%] md:max-w-full"}>
-        <p>{ directGetterChannelInfo(currentChannelId()).description }</p>
-      </span>
+          {/* チャンネルの閲覧権限がある時の錠前アイコン */}
+          <Show when={directGetterChannelInfo(currentChannelId()).ChannelViewableRole.length !== 0}>
+            <HoverCard>
+              <HoverCardTrigger>
+                <IconLock class={"shrink-0 cursor-help"} size={"18"} />
+              </HoverCardTrigger>
+              <HoverCardContent>
+                ロールによる閲覧制限がかかっているチャンネルです
+              </HoverCardContent>
+            </HoverCard>
+          </Show>
 
-      <span class={"ml-auto"}>
-        <ChannelManage channelId={currentChannelId()} />
-      </span>
-    </Card>
+          <span class={"shrink line-clamp-1"}>
+            <p>{directGetterChannelInfo(currentChannelId()).name}</p>
+          </span>
+          <p class="text-gray-400 mx-1"> | </p>
+          <span class={"shrink-[2] grow-0 line-clamp-1 max-w-[50%] md:max-w-full"}>
+            <p>{directGetterChannelInfo(currentChannelId()).description}</p>
+          </span>
+
+          <span class={"ml-auto"}>
+            <ChannelManage channelId={currentChannelId()} />
+          </span>
+        </Card>
+      </Match>
+
+      <Match when={storeChannelFetchStatus[currentChannelId()] === "NOT_FOUND"}>
+        <Card class="py-3 px-5 flex items-center w-full gap-2">
+          <SidebarTriggerWithDot />
+          <span class={"shrink line-clamp-1"}>
+            <p class="italic">チャンネルが見つかりません {currentChannelId()} {storeChannelFetchStatus[currentChannelId()]}</p>
+          </span>
+        </Card>
+      </Match>
+    </Switch>
   );
 }
