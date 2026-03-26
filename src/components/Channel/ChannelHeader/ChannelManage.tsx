e0d79d89dab1b339123005baefc103428c46928f
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2026-03-26 23:26:18 +0900

    change - チャンネル情報モーダルの表示調整

--- src/components/Channel/ChannelHeader/ChannelManage.tsx
+++ src/components/Channel/ChannelHeader/ChannelManage.tsx
@@ -1,4 +1,4 @@
-import {IconInfoCircle, IconPencil} from "@tabler/icons-solidjs";
+import { IconInfoCircle, IconPencil } from "@tabler/icons-solidjs";
 import { Button } from "~/components/ui/button.tsx";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTrigger } from "~/components/ui/dialog.tsx";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs.tsx";
@@ -6,35 +6,39 @@ import { getRolePower } from "~/stores/MyUserinfo.ts";
 import ChannelInfo from "./ChannelManage/ChannelInfos.tsx";
 import ChannelMembers from "./ChannelManage/ChannelMembers.tsx";
 
-export default function ChannelManage(props: {channelId: string}) {
+export default function ChannelManage(props: { channelId: string }) {
 
   return (
     <Dialog>
       <DialogTrigger>
         <Button variant={"secondary"} class={"w-9 h-9"}>
-          { getRolePower("manageChannel") ? <IconPencil /> : <IconInfoCircle /> }
+          {getRolePower("manageChannel") ? <IconPencil /> : <IconInfoCircle />}
         </Button>
       </DialogTrigger>
       <DialogContent class={"pt-10 w-full"}>
         <DialogHeader>
           <p>チャンネル情報</p>
         </DialogHeader>
-        <DialogDescription class={"flex flex-col gap-2"}>
+        <DialogDescription class={"overflow-x-hidden w-full"}>
 
-          <Tabs defaultValue="info" class="w-full h-[450px]">
-            <TabsList>
+          <Tabs defaultValue="info" class="w-full h-[450px] flex flex-col">
+            <TabsList class="w-fit">
               <TabsTrigger value="info">概要</TabsTrigger>
               <TabsTrigger value="users">参加者</TabsTrigger>
             </TabsList>
 
-            <TabsContent value="info">
-              <ChannelInfo channelId={props.channelId} />
-            </TabsContent>
-            <TabsContent value="users">
-              <ChannelMembers channelId={props.channelId} />
-            </TabsContent>
+            <hr class="mt-2" />
+
+            <div class="overflow-x-hidden overflow-y-auto w-full">
+              <TabsContent value="info">
+                <ChannelInfo channelId={props.channelId} />
+              </TabsContent>
+              <TabsContent value="users">
+                <ChannelMembers channelId={props.channelId} />
+              </TabsContent>
+            </div>
           </Tabs>
-          
+
         </DialogDescription>
       </DialogContent>
     </Dialog>
--- src/components/Channel/ChannelHeader/ChannelManage/ChannelInfos.tsx
+++ src/components/Channel/ChannelHeader/ChannelManage/ChannelInfos.tsx
@@ -9,47 +9,47 @@ import RoleLinker from "~/components/unique/RoleLinker.tsx";
 import { directGetterChannelInfo } from "~/stores/ChannelInfo.ts";
 import { getRolePower } from "~/stores/MyUserinfo.ts";
 
-export default function ChannelInfo(props: {channelId: string}) {
+export default function ChannelInfo(props: { channelId: string }) {
   const [editName, setEditName] = createSignal(false);
-    const [editDescription, setEditDescription] = createSignal(false);
-    const [newName, setNewName] = createSignal("");
-    const [newDescription, setNewDescription] = createSignal("");
-    const [newRoles, setNewRoles] = createSignal<string[]>([]);
-  
-    //閲覧可能ロールが変更されたかどうか
-    const roleIsDiff = () => {
-      const oldRoles = directGetterChannelInfo(props.channelId).ChannelViewableRole.map((r)=>r.roleId).join(",");
-      return oldRoles !== newRoles().join(",");
-    }
-  
-    /**
-     * チャンネル情報を更新する
-     */
-    const updateChannel = () => {
-      POST_CHANNEL_UPDATE({
-        name: newName()!=="" ? newName() : undefined,
-        description: newDescription()!=="" ? newDescription() : undefined,
-        viewableRole: roleIsDiff() ? newRoles() : undefined,
-        channelId: props.channelId
+  const [editDescription, setEditDescription] = createSignal(false);
+  const [newName, setNewName] = createSignal("");
+  const [newDescription, setNewDescription] = createSignal("");
+  const [newRoles, setNewRoles] = createSignal<string[]>([]);
+
+  //閲覧可能ロールが変更されたかどうか
+  const roleIsDiff = () => {
+    const oldRoles = directGetterChannelInfo(props.channelId).ChannelViewableRole.map((r) => r.roleId).join(",");
+    return oldRoles !== newRoles().join(",");
+  }
+
+  /**
+   * チャンネル情報を更新する
+   */
+  const updateChannel = () => {
+    POST_CHANNEL_UPDATE({
+      name: newName() !== "" ? newName() : undefined,
+      description: newDescription() !== "" ? newDescription() : undefined,
+      viewableRole: roleIsDiff() ? newRoles() : undefined,
+      channelId: props.channelId
+    })
+      .then((r) => {
+        console.log(r);
+        setEditDescription(false);
+        setEditName(false);
+        //更新後の閲覧可能ロールを格納
+        const roleIdArr = [...directGetterChannelInfo(props.channelId).ChannelViewableRole];
+        setNewRoles(roleIdArr.map((r) => r.roleId));
       })
-        .then((r) => {
-          console.log(r);
-          setEditDescription(false);
-          setEditName(false);
-          //更新後の閲覧可能ロールを格納
-          const roleIdArr = [...directGetterChannelInfo(props.channelId).ChannelViewableRole];
-          setNewRoles(roleIdArr.map((r)=>r.roleId));
-        })
-        .catch((e) => {
-          console.error("ChannelManage :: updateChannel : e", e);
-        })
-    }
-  
-    //チャンネルを移動するごとに閲覧可能ロールを更新
-    createEffect(on(() => props.channelId, () => {
-      const roleIdArr = [...directGetterChannelInfo(props.channelId).ChannelViewableRole];
-      setNewRoles(roleIdArr.map((r)=>r.roleId));
-    }));
+      .catch((e) => {
+        console.error("ChannelManage :: updateChannel : e", e);
+      })
+  }
+
+  //チャンネルを移動するごとに閲覧可能ロールを更新
+  createEffect(on(() => props.channelId, () => {
+    const roleIdArr = [...directGetterChannelInfo(props.channelId).ChannelViewableRole];
+    setNewRoles(roleIdArr.map((r) => r.roleId));
+  }));
 
   return (
     <div class="mt-2">
@@ -64,21 +64,21 @@ export default function ChannelInfo(props: {channelId: string}) {
                   <TextFieldInput
                     placeholder={"チャンネル名"}
                     value={directGetterChannelInfo(props.channelId).name}
-                    onInput={(e)=>setNewName(e.currentTarget.value)}
+                    onInput={(e) => setNewName(e.currentTarget.value)}
                   />
                 </TextField>
                 <Button onClick={updateChannel} class={"ml-auto h-10 w-10"}><IconCheck /></Button>
-                <Button onClick={()=>setEditName(false)} class={"ml-auto h-10 w-10"} variant={"outline"}><IconX /></Button>
+                <Button onClick={() => setEditName(false)} class={"ml-auto h-10 w-10"} variant={"outline"}><IconX /></Button>
               </div>
               :
               <div class={"flex items-center overflow-x-scroll gap-1"}>
                 <span class={"shrink max-w-48 md:max-w-80 overflow-x-scroll"}>
-                  <p class={"text-2xl truncate"}>{ directGetterChannelInfo(props.channelId).name ?? "ロード中..." }</p>
+                  <p class={"text-2xl truncate"}>{directGetterChannelInfo(props.channelId).name ?? "ロード中..."}</p>
                 </span>
                 { //権限ある場合の編集ボタン
-                  getRolePower("manageChannel") 
+                  getRolePower("manageChannel")
                   &&
-                  <Button onClick={()=>setEditName(true)} variant={"outline"} class={"ml-auto border rounded-md h-10 w-10"}><IconPencil /></Button>
+                  <Button onClick={() => setEditName(true)} variant={"outline"} class={"ml-auto border rounded-md h-10 w-10"}><IconPencil /></Button>
                 }
               </div>
           }
@@ -89,28 +89,28 @@ export default function ChannelInfo(props: {channelId: string}) {
         <Label class={"text-muted-foreground"}>概要</Label>
         {
           !editDescription()
-          ?
-            <div class={"pt-3 pb-12 relative max-h-64 overflow-y-auto"}>
-                <p>{ directGetterChannelInfo(props.channelId).description }</p>
-                { directGetterChannelInfo(props.channelId).description==="" && <p class={"text-muted-foreground"}>概要が空です。</p> }
+            ?
+            <div class={"pt-3 pb-12 relative max-h-64 overflow-y-auto whitespace-pre-wrap break-words"}>
+              <p>{directGetterChannelInfo(props.channelId).description}</p>
+              {directGetterChannelInfo(props.channelId).description === "" && <p class={"text-muted-foreground"}>概要が空です。</p>}
 
-                { //権限ある場合の編集ボタン
-                  getRolePower("manageChannel")
-                  &&
-                  <Button onClick={()=>setEditDescription(true)} class={"absolute bottom-2 right-0 border rounded-md h-10 w-10"} variant={"outline"} ><IconPencil /></Button>
-                }
+              { //権限ある場合の編集ボタン
+                getRolePower("manageChannel")
+                &&
+                <Button onClick={() => setEditDescription(true)} class={"absolute bottom-2 right-0 border rounded-md h-10 w-10"} variant={"outline"} ><IconPencil /></Button>
+              }
             </div>
-          :
+            :
             <div class={"flex flex-col gap-1"}>
               <TextField class={"max-h-64 overflow-y-auto"}>
                 <TextFieldTextArea
                   value={directGetterChannelInfo(props.channelId).description}
-                  onInput={(e)=>setNewDescription(e.currentTarget.value)}
+                  onInput={(e) => setNewDescription(e.currentTarget.value)}
                 />
               </TextField>
               <div class={"w-fit ml-auto p-2 flex items-center gap-1"}>
                 <Button onClick={updateChannel} class={"border rounded-md h-10 w-10"} ><IconCheck /></Button>
-                <Button onClick={()=>setEditDescription(false)} class={"border rounded-md h-10 w-10"} variant={"outline"} ><IconX /></Button>
+                <Button onClick={() => setEditDescription(false)} class={"border rounded-md h-10 w-10"} variant={"outline"} ><IconX /></Button>
               </div>
             </div>
         }
@@ -121,7 +121,7 @@ export default function ChannelInfo(props: {channelId: string}) {
       <Label>閲覧できるロール  {roleIsDiff() && "*"}</Label>
       <RoleLinker
         roles={newRoles()}
-        onUpdate={(roles)=>setNewRoles(roles)}
+        onUpdate={(roles) => setNewRoles(roles)}
       />
       { //権限がある場合のロール更新ボタン
         getRolePower("manageChannel")
