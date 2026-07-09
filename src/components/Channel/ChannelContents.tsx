564b6aaf32f63a92f419c996fc701bee7993f01a
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2026-07-09 23:03:14 +0900

    remove - 不要なimport

--- src/components/Channel/ChannelContents.tsx
+++ src/components/Channel/ChannelContents.tsx
@@ -6,7 +6,7 @@ import { storeClientConfig } from "~/stores/ClientConfig.ts";
 import { Button } from "../ui/button.tsx";
 import { IconArrowDown } from "@tabler/icons-solidjs";
 import MessageDisplay from "./ChannelContent/MessageDisplay.tsx";
-import { setStoreMessageReadTime, storeMessageReadTime } from "~/stores/Readtime.ts";
+import { storeMessageReadTime } from "~/stores/Readtime.ts";
 import { storeMyUserinfo } from "~/stores/MyUserinfo.ts";
 import { api } from "~/api/index.ts";
 import { produce } from "solid-js/store";
