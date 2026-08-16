3620b1dbd716b0248b7c162717a829f855958fc0
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2026-08-16 11:36:23 +0900

    add - 土台だけ

--- src/routes/members.tsx
+++ src/routes/members.tsx
@@ -1,7 +1,17 @@
+import { Card } from "~/components/ui/card";
+import SidebarTriggerWithDot from "~/components/unique/SidebarTriggerWithDot";
+
 export default function Members() {
   return (
-    <div>
-      ここでメンバー表示
+    <div class={"p-2 flex flex-col gap-2"}>
+      <Card class="w-full py-3 px-5 flex items-center gap-2">
+        <SidebarTriggerWithDot />
+        <p>メンバー一覧</p>
+      </Card>
+
+      <div>
+        asdf
+      </div>
     </div>
   );
 }
