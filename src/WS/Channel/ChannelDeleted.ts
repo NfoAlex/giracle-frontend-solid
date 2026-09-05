a723555b4d346b119baab574964733a90009fa8b
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2026-09-05 15:45:26 +0900

    fix - チャンネルWS残骸消去・store更新

--- src/WS/Channel/ChannelDeleted.ts
+++ src/WS/Channel/ChannelDeleted.ts
@@ -1,23 +1,38 @@
+import { produce } from "solid-js/store";
+import {
+  setStoreHasNewMessage,
+  storeHasNewMessage,
+} from "~/stores/HasNewMessage.store.ts";
+import { setStoreHistory, storeHistory } from "~/stores/History.store.ts";
 import {
   setStoreMyUserinfo,
   storeMyUserinfo,
 } from "~/stores/MyUserinfo.store.ts";
 
 export default function WSChannelDeleted(dat: { channelId: string }) {
-  //console.log("WSChannelDeleted :: triggered dat->", dat);
-
-  const myJoinedChannel = storeMyUserinfo.ChannelJoin;
-
-  //チャンネル参加しているなら削除
-  if (myJoinedChannel.map((cj) => cj.channelId).includes(dat.channelId)) {
-    setStoreMyUserinfo((prev) => {
-      const ChannelJoinFiltered = prev.ChannelJoin.filter(
-        (cj) => cj.channelId !== dat.channelId,
-      );
-      return {
-        ...prev,
-        ChannelJoin: ChannelJoinFiltered,
-      };
-    });
+  //履歴削除
+  if (storeHistory[dat.channelId] !== undefined) {
+    setStoreHistory(
+      produce((prev) => {
+        delete prev[dat.channelId];
+      }),
+    );
   }
+
+  //新着削除
+  if (storeHasNewMessage[dat.channelId] !== undefined) {
+    setStoreHasNewMessage(
+      produce((prev) => {
+        delete prev[dat.channelId];
+      }),
+    );
+  }
+
+  //未参加なら更新不要
+  if (!storeMyUserinfo.ChannelJoin.some((cj) => cj.channelId === dat.channelId))
+    return;
+
+  setStoreMyUserinfo("ChannelJoin", (prev) =>
+    prev.filter((cj) => cj.channelId !== dat.channelId),
+  );
 }
--- src/WS/Channel/ChannelJoined.ts
+++ src/WS/Channel/ChannelJoined.ts
@@ -1,20 +1,10 @@
-import {
-  setStoreMyUserinfo,
-  storeMyUserinfo,
-} from "~/stores/MyUserinfo.store.ts";
+import { setStoreMyUserinfo } from "~/stores/MyUserinfo.store.ts";
 
 export default function WSChannelJoined(dat: { channelId: string }) {
-  //console.log("WSChannelJoined :: triggered dat->", dat);
-
-  const myJoinedChannel = storeMyUserinfo.ChannelJoin;
-
-  //チャンネル参加を確認して自ユーザーStoreを更新
-  if (!myJoinedChannel.some((cj) => cj.channelId === dat.channelId)) {
-    setStoreMyUserinfo((prev) => {
-      return {
-        ...prev,
-        ChannelJoin: [...prev.ChannelJoin, { channelId: dat.channelId }],
-      };
-    });
-  }
+  // setter内判定→連続受信race防止
+  setStoreMyUserinfo("ChannelJoin", (prev) =>
+    prev.some((cj) => cj.channelId === dat.channelId)
+      ? prev
+      : [...prev, { channelId: dat.channelId }],
+  );
 }
