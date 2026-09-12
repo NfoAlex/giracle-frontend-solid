32011fef61a5f3020933de95a62601d994501c06
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2026-09-13 04:07:31 +0900

    fix - やべ
    IBot

--- src/types/Server.ts
+++ src/types/Server.ts
@@ -11,6 +11,39 @@ export interface IServer {
   defaultJoinChannel: IChannel[];
 }
 
+export interface IBot {
+  approveStatus: "APPROVED" | "BLOCKED" | "DENIED" | "PENDING";
+  botDescription: string | null;
+  botName: string;
+  canFetchRoleinfo: boolean;
+  canFetchUserinfo: boolean;
+  canManageServerConfig: boolean;
+  canManageUser: boolean;
+  canReadMessage: boolean;
+  canSendMessage: boolean;
+  channelPermissions: IBotChannelPermission[];
+  createdAt: Date;
+  createdBy: string;
+  id: string;
+  remoteUserId: string;
+  useAllChannel: boolean;
+  user: {
+    createdAt: Date;
+    id: string;
+    isBanned: boolean;
+    isBot: boolean;
+    isDeleted: boolean;
+    name: string | null;
+    selfIntroduction: string;
+  };
+}
+
+export interface IBotChannelPermission {
+  botId: string;
+  channelId: string;
+  id: number;
+}
+
 export interface IInvite {
   inviteCode: string;
   id: number;
