2aa68dcd601973e2c3e776731d74ac9000ccb74e
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2026-09-26 03:10:27 +0900

    change - テキスト調整

--- src/components/Config/ConfigBot/ConfigBotDetail.tsx
+++ src/components/Config/ConfigBot/ConfigBotDetail.tsx
@@ -210,7 +210,10 @@ export default function ConfigBotDetail(props: { returnToListProxy: () => void; 
           <p class="mt-4 font-medium">権限</p>
           <Card class="shrink-0 p-4 flex flex-col gap-4">
             <div class="flex items-center w-full">
-              <p>他人・自分のメッセージを取得できる</p>
+              <span class="flex flex-col">
+                <p>他人・自分のメッセージを取得できる</p>
+                <p class="text-xs">リアルタイムでのメッセージ受信は権限関係無く可能です。</p>
+              </span>
               <Switch
                 checked={bot()!.canReadMessage ?? false}
                 onChange={(v) => setBot(b => ({ ...b!, canReadMessage: v }))}
@@ -223,7 +226,7 @@ export default function ConfigBotDetail(props: { returnToListProxy: () => void; 
             </div>
             <hr />
             <div class="flex items-center w-full">
-              <p>自分のメッセージを送信・編集できる</p>
+              <p>Botとして自分のメッセージを送信・編集できる</p>
               <Switch
                 checked={bot()!.canSendMessage ?? false}
                 onChange={(v) => setBot(b => ({ ...b!, canSendMessage: v }))}
